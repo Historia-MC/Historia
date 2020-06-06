b6999c2fe8b6d1b5eabd08ed2cf8510e172b3118
boooiil <boooiil@outlook.com>
2020-06-06 13:29:10 -0400

    Notify on message command.

--- src/main/java/dev/boooiil/historia/Main.java
+++ src/main/java/dev/boooiil/historia/Main.java
@@ -3,6 +3,7 @@ package dev.boooiil.historia;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import dev.boooiil.historia.commands.Ignite;
+import dev.boooiil.historia.commands.Message;
 
 public class Main extends JavaPlugin {
 
@@ -13,20 +14,30 @@ public class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
+
+        //Disabled due to being beginner commands and not having a use.
         //this.getCommand("Launch").setExecutor(new Launch());
         //this.getCommand("Help").setExecutor(new Help());
         //this.getCommand("Heal").setExecutor(new Heal());
+
         this.getCommand("Ignite").setExecutor(new Ignite());
+        this.getCommand("Message").setExecutor(new Message());
 
         //Disabled due to complication.
         //this.getServer().getPluginManager().registerEvents(new InventoryClick(), this);
+
+        //Disabled due to command not being part of chat event.
+        //this.getServer().getPluginManager().registerEvents(new AsyncPlayerChat(), this);
 
         this.getServer().getPluginManager().registerEvents(new PlayerBucketInteract(), this);
         this.getServer().getPluginManager().registerEvents(new PlayerConsumeItem(), this);
         this.getServer().getPluginManager().registerEvents(new PlayerDeath(), this);
         this.getServer().getPluginManager().registerEvents(new PlayerEntityInteract(), this);
         this.getServer().getPluginManager().registerEvents(new PlayerInteract(), this);
+
+        //Disabled due to expiry not being finished.
         //this.getServer().getPluginManager().registerEvents(new PlayerItemHeld(), this);
+
         this.getServer().getPluginManager().registerEvents(new ProjectileLaunch(), this);
         this.getServer().getPluginManager().registerEvents(new ProjectileHit(), this);
         this.saveConfig(); getLogger().info("Plugin enabled.");
---
+++ src/main/java/dev/boooiil/historia/commands/Message.java
@@ -0,0 +1,18 @@
+package dev.boooiil.historia.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Message implements CommandExecutor{
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+            if (!sender.hasPermission("essentials.msg")) {
+                sender.sendMessage("§7[§9Historia§7] Please use /letter <message> and /post <user>!");
+                return true;
+            }
+        }
+        return false;
+    }
+}
