a234bbcda75cea4213078d0872aecdf210b73546
boooiil <boooiil@outlook.com>
2020-06-25 18:35:15 -0400

    ability to shear chickens

---
+++ src/main/java/dev/boooiil/historia/misc/ChickenShearing.java
@@ -0,0 +1,44 @@
+package dev.boooiil.historia.misc;
+
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.entity.Ageable;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public class ChickenShearing {
+    
+    public static void shearChicken(Player player, Entity entity) {
+
+        if (player.getInventory().getItemInMainHand().getType().equals(Material.SHEARS)) {
+    
+            Ageable chickenAge = (Ageable) entity;
+
+            if (chickenAge.getAge() < 0) return;
+            else {
+                
+                entity.remove();
+
+                Ageable newChicken = (Ageable) player.getWorld().spawnEntity(entity.getLocation(), EntityType.CHICKEN);
+                ItemStack feathers = new ItemStack(Material.FEATHER); feathers.setAmount((int) (Math.random() * 3) + 1);
+                
+                newChicken.setBaby(); player.getWorld().dropItemNaturally(entity.getLocation(), feathers);
+                player.giveExp(2);
+
+                ItemStack shears = player.getInventory().getItemInMainHand();
+                Short calculatedDurability = (short) ( shears.getDurability() + 10);
+
+                if (calculatedDurability >= (int) shears.getType().getMaxDurability()) {
+                    player.playSound(player.getLocation(), Sound.ENTITY_SHEEP_SHEAR, 15, 1);
+                    player.getInventory().setItemInMainHand(new ItemStack(Material.AIR)); player.playSound(player.getLocation(), Sound.ENTITY_ITEM_BREAK, 15, 1);
+                } else {
+                    shears.setDurability(calculatedDurability);
+                    player.playSound(player.getLocation(), Sound.ENTITY_SHEEP_SHEAR, 15, 1);
+                }
+                
+            }
+        }
+    }
+}
