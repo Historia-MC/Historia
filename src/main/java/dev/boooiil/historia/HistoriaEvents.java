84c1b75ba9adcd358c8961aa53f8d01ceaeaeb2a
boooiil <boooiil@outlook.com>
2020-06-25 18:34:45 -0400

    added entity interaction events

--- src/main/java/dev/boooiil/historia/HistoriaEvents.java
+++ src/main/java/dev/boooiil/historia/HistoriaEvents.java
@@ -1,15 +1,17 @@
 package dev.boooiil.historia;
 
-import org.bukkit.entity.Player;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.entity.ProjectileLaunchEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerItemHeldEvent;
-import org.bukkit.inventory.ItemStack;
 
+import dev.boooiil.historia.alerts.BoatNotify;
 import dev.boooiil.historia.expiry.ExpiryManager;
+import dev.boooiil.historia.misc.ChickenShearing;
 import dev.boooiil.historia.misc.FlameArrowHandler;
 
 public class HistoriaEvents implements Listener {
@@ -30,14 +32,12 @@ public class HistoriaEvents implements Listener {
     @EventHandler
     public void onPlayerItemHeldSwitch(PlayerItemHeldEvent event) {
 
-        Player player = event.getPlayer();
-        ItemStack newItem = player.getInventory().getItem(event.getNewSlot());
-        ItemStack previousItem = player.getInventory().getItem(event.getPreviousSlot());
-        Integer newItemSlot = event.getNewSlot();
-        Integer oldItemSlot = event.getPreviousSlot();
-
-        if (previousItem != null) ExpiryManager.initiate(previousItem, player, oldItemSlot);
-        if (newItem != null) ExpiryManager.initiate(newItem, player, newItemSlot);
+        if (event.getPlayer().getInventory().getItem(event.getPreviousSlot()) != null) {
+            ExpiryManager.initiate(event.getPlayer().getInventory().getItem(event.getPreviousSlot()), event.getPlayer(), event.getNewSlot());
+        }
+        if (event.getPlayer().getInventory().getItem(event.getNewSlot()) != null) {
+            ExpiryManager.initiate(event.getPlayer().getInventory().getItem(event.getNewSlot()), event.getPlayer(), event.getNewSlot());
+        } 
 
     }
 
@@ -48,4 +48,11 @@ public class HistoriaEvents implements Listener {
         
     }
 
+    @EventHandler
+    public void onBoatInteract(PlayerInteractEntityEvent event) {
+
+        if (event.getRightClicked().getType() == EntityType.BOAT) BoatNotify.boatNotify(event.getHand(), event.getPlayer(), event.getRightClicked());
+        if (event.getRightClicked().getType() == EntityType.CHICKEN) ChickenShearing.shearChicken(event.getPlayer(), event.getRightClicked());
+        
+    }
 }
