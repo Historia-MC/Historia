7489be911f837d0f45c8f6071f55c43fd87903b7
boooiil <boooiil@outlook.com>
2020-06-26 15:59:40 -0400

    merged Interact, ConsumeItem, and Death events

--- src/main/java/dev/boooiil/historia/HistoriaEvents.java
+++ src/main/java/dev/boooiil/historia/HistoriaEvents.java
@@ -1,18 +1,24 @@
 package dev.boooiil.historia;
 
+import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.entity.ProjectileLaunchEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerBucketFillEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
 import org.bukkit.event.player.PlayerItemHeldEvent;
 
 import dev.boooiil.historia.alerts.BoatNotify;
+import dev.boooiil.historia.alerts.DeathNotify;
 import dev.boooiil.historia.expiry.ExpiryManager;
 import dev.boooiil.historia.misc.ChickenShearing;
 import dev.boooiil.historia.misc.FlameArrowHandler;
+import dev.boooiil.historia.misc.PreventLavaPickup;
 
 public class HistoriaEvents implements Listener {
 
@@ -53,6 +59,26 @@ public class HistoriaEvents implements Listener {
 
         if (event.getRightClicked().getType() == EntityType.BOAT) BoatNotify.boatNotify(event.getHand(), event.getPlayer(), event.getRightClicked());
         if (event.getRightClicked().getType() == EntityType.CHICKEN) ChickenShearing.shearChicken(event.getPlayer(), event.getRightClicked());
+        
+    }
+
+    @EventHandler
+    public void onPickupLava(PlayerBucketFillEvent event) {
+        
+        if (event.getBlock().getType() == Material.LAVA) {
+            if (PreventLavaPickup.onPickup(event.getPlayer())) event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onPlayerEat(PlayerItemConsumeEvent event) {
+        if (ExpiryManager.handleExpiredFood(event.getItem(), event.getPlayer())) event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onDeath(PlayerDeathEvent event) {
+        
+        DeathNotify.deathAlert(event.getEntity(), event.getDeathMessage());
         
     }
 }
