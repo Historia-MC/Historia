5aebbfd517999399885a8fed41641717d93b9cf8
boooiil <boooiil@outlook.com>
2020-06-28 05:52:20 -0400

    prep class manager for class system

---
+++ src/main/java/dev/boooiil/historia/classes/ClassManager.java
@@ -0,0 +1,24 @@
+package dev.boooiil.historia.classes;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+public class ClassManager {
+    
+    public void initiate(Player player) {
+        //do check for player stats
+
+        //example stats
+        player.setExhaustion(30);
+        //player.setFoodLevel(400);
+        player.setHealthScale(10.5);
+        player.setWalkSpeed((float) 0.2);
+        //player.getScoreboard().registerNewTeam("test").setOption(Team.Option.NAME_TAG_VISIBILITY, Team.OptionStatus.NEVER);
+        player.getScoreboard().getTeam("test").addEntry(player.getDisplayName());
+
+        for (Player online : Bukkit.getOnlinePlayers()) {
+            player.showPlayer(Bukkit.getPluginManager().getPlugin("Historia"), online);
+        }
+    }
+
+}
