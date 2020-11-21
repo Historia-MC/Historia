760c0876ac99ab88e16ee349896ed3cad8c65da9
boooiil <boooiil@outlook.com>
2020-11-21 13:57:31 -0500

    add class / weapon / armor configs

--- src/main/java/dev/boooiil/historia/Config.java
+++ src/main/java/dev/boooiil/historia/Config.java
@@ -1,43 +1,85 @@
 package dev.boooiil.historia;
 
 import java.util.List;
+import java.util.Map;
 
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 
 public class Config {
 
-    String name;
-    Integer health;
-    Integer baseFood;
-    Integer exhaustion;
-    Integer saturation;
-    Integer speed;
-    List<?> skills;
-    List<?> foods;
-    List<?> effects;
+    // Gets the plugins config file.
+    private FileConfiguration cfg = Bukkit.getPluginManager().getPlugin("Historia").getConfig();
+    private String root;
 
-    //Gets the plugins config file.
-    FileConfiguration cfg = Bukkit.getPluginManager().getPlugin("Historia").getConfig();
+    public boolean empty = false;
 
-    //Class constructor
-    Config(String className) {
+    // Class information
+    public String className = null;
+    public Integer health = 0;
+    public Integer baseFood = 0;
+    public Integer exhaustion = 0;
+    public Integer saturation = 0;
+    public Integer speed = 0;
 
-        //If a class name was provided.
-        if (className != null) {
+    // Weapon / Armor information
+    public String weaponName = null;
+    public String armorName = null;
+    public Integer weaponDamage = 0;
+    public Integer armorValue = 0;
 
-            //Assign variables based on that class name.
-            name = className;
-            health = cfg.getInt(className + ".stats.health");
-            baseFood = cfg.getInt(className + ".stats.baseFood");
-            exhaustion =  cfg.getInt(className + ".stats.exhaustion");
-            saturation = cfg.getInt(className + ".stats.saturation");
-            speed = cfg.getInt(className + ".stats.speed");
+    // Known Lists
+    private List<String> classes = cfg.getStringList("classes.list");
+    private List<String> armor = cfg.getStringList("weapons.list");
+    private List<String> weapons = cfg.getStringList("armor.list");
+
+    // Unknown Lists
+    private List<?> skills;
+    private List<?> foods;
+    private List<?> effects;
+
+    // Class constructor
+    public Config(String query) {
+
+        // If a class name was provided.
+        if (classes.contains(query)) {
+
+            root = "classes.";
+
+            // Assign variables based on that class name.
+            className = query;
+            health = cfg.getInt(root + className + ".stats.health");
+            baseFood = cfg.getInt(root + className + ".stats.baseFood");
+            exhaustion = cfg.getInt(root + className + ".stats.exhaustion");
+            saturation = cfg.getInt(root + className + ".stats.saturation");
+            speed = cfg.getInt(root + className + ".stats.speed");
         }
 
-        foods = cfg.getList("foods");
+        else if (query.equals("foods")) {
 
-        effects = cfg.getList("effects");
+            foods = cfg.getList("foods.list");
+
+            effects = cfg.getList("foods.effects");
+
+        }
+
+        else if (weapons.contains(query)) {
+
+            root = "weapons.";
+
+            weaponName = query;
+            weaponDamage = cfg.getInt(root + query + ".damage");
+
+        }
+
+        else if (armor.contains(query)) {
+
+            root = "armor.";
+
+            armorName = query;
+            armorValue = cfg.getInt(root + query + ".armor");
+
+        } else empty = true;
 
     }
 
