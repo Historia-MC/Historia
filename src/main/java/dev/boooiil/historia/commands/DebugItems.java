5d516f238ed5f4d619c6ad7cdd2e6a52d293fa67
boooiil <boooiil@outlook.com>
2020-11-21 19:02:42 -0500

    fixed error on empty armor / hand

--- src/main/java/dev/boooiil/historia/commands/DebugItems.java
+++ src/main/java/dev/boooiil/historia/commands/DebugItems.java
@@ -1,12 +1,16 @@
 package dev.boooiil.historia.commands;
 
+import com.sk89q.worldedit.event.platform.PlayerInputEvent;
+
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.meta.ItemMeta;
 
 public class DebugItems implements CommandExecutor {
@@ -17,14 +21,26 @@ public class DebugItems implements CommandExecutor {
 
                         Player player = (Player) sender;
 
-                        ItemStack itemInMainHand = player.getInventory().getItemInMainHand();
-                        ItemStack itemInOffHand = player.getInventory().getItemInOffHand();
-                        ItemStack itemHelmet = player.getInventory().getHelmet();
-                        ItemStack itemChestPlate = player.getInventory().getChestplate();
-                        ItemStack itemLeggings = player.getInventory().getLeggings();
-                        ItemStack itemBoots = player.getInventory().getBoots();
+                        PlayerInventory playerInventory = player.getInventory();
 
-                        ItemMeta itemInMainHandMeta = itemInMainHand.getItemMeta();
+                        ItemStack[] playerArmor = playerInventory.getArmorContents();
+
+                        ItemStack helmet = playerArmor[0] != null ? playerArmor[0] : new ItemStack(Material.AIR);
+                        ItemStack chestplate = playerArmor[1] != null ? playerArmor[1] : new ItemStack(Material.AIR);
+                        ItemStack leggings = playerArmor[2] != null ? playerArmor[2] : new ItemStack(Material.AIR);
+                        ItemStack boots = playerArmor[3] != null ? playerArmor[3] : new ItemStack(Material.AIR);
+
+                        ItemStack mainHand = playerInventory.getItemInMainHand();
+                        ItemStack offHand = playerInventory.getItemInOffHand();
+
+                        //ItemStack itemInMainHand = player.getInventory().getItemInMainHand();
+                        //ItemStack itemInOffHand = player.getInventory().getItemInOffHand();
+                        //ItemStack itemHelmet = player.getInventory().getHelmet();
+                        //ItemStack itemChestPlate = player.getInventory().getChestplate();
+                        //ItemStack itemLeggings = player.getInventory().getLeggings();
+                        //ItemStack itemBoots = player.getInventory().getBoots();
+
+                        //ItemMeta itemInMainHandMeta = itemInMainHand.getItemMeta();
                         /*
                         ItemMeta itemInOffHandMeta = itemInOffHand.getItemMeta();
                         ItemMeta itemHelmetMeta = itemHelmet.getItemMeta();
@@ -40,11 +56,22 @@ public class DebugItems implements CommandExecutor {
                         // !
                         // !
 
-                        String messageToSend = (itemInMainHandMeta.getDisplayName() + " : " + itemInMainHandMeta.getLocalizedName());
+                        //Send armors.
+                        if (helmet.getType() != Material.AIR) player.sendMessage("Helmet: " + helmet.getType() + " " + helmet.getItemMeta().getDisplayName() + " " + helmet.getItemMeta().getLocalizedName());
+                        if (chestplate.getType() != Material.AIR) player.sendMessage("Chestplate: " + chestplate.getType() + " " + chestplate.getItemMeta().getDisplayName() + " " + chestplate.getItemMeta().getLocalizedName());
+                        if (leggings.getType() != Material.AIR) player.sendMessage("Leggings: " + leggings.getType() + " " + leggings.getItemMeta().getDisplayName() + " " + leggings.getItemMeta().getLocalizedName());
+                        if (boots.getType() != Material.AIR) player.sendMessage("Boots: " + boots.getType() + " " + boots.getItemMeta().getDisplayName() + " " + boots.getItemMeta().getLocalizedName());
+
+                        //Send off / main hand.
+                        if (mainHand.getType() != Material.AIR) player.sendMessage("Main Hand: " + mainHand.getType() + " " + mainHand.getItemMeta().getDisplayName() + " " + mainHand.getItemMeta().getLocalizedName());
+                        if (offHand.getType() != Material.AIR) player.sendMessage("Off Hand: " + offHand.getType() + " " + offHand.getItemMeta().getDisplayName() + " " + offHand.getItemMeta().getLocalizedName());
+
+
+                        //String messageToSend = (itemInMainHandMeta.getDisplayName() + " : " + itemInMainHandMeta.getLocalizedName());
 
                         // Bukkit.getServer().broadcastMessage(messageToSend); //send player's helmet display name and localized name
                         
-                        player.sendMessage(messageToSend); 
+                        //player.sendMessage(messageToSend); 
 
                         return true;
 
