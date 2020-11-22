9401bb93c1edbc9288648e42cf928d33596b305f
boooiil <boooiil@outlook.com>
2020-11-21 19:08:15 -0500

    base damage manager for new weapons / armors

--- src/main/java/dev/boooiil/historia/tools/DamageManager.java
+++ src/main/java/dev/boooiil/historia/tools/DamageManager.java
@@ -1,13 +1,18 @@
 package dev.boooiil.historia.tools;
 
+import com.sk89q.worldedit.antlr.ExpressionParser.AssignmentOperatorContext;
+
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.entity.EntityDamageByBlockEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+import org.bukkit.inventory.EntityEquipment;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.PlayerInventory;
 
 import dev.boooiil.historia.Config;
 
@@ -17,83 +22,117 @@ public class DamageManager {
     private boolean sweepAttack;
     private boolean arrowAttack;
 
+    private Double baseDamage;
+
     public void initiate(EntityDamageEvent damageEvent) {
 
+        if (damageEvent instanceof EntityDamageByBlockEvent) return;
+        if (!(damageEvent instanceof EntityDamageByEntityEvent)) return;
+
+        EntityDamageByEntityEvent entityDamage = (EntityDamageByEntityEvent) damageEvent;
+
         DamageCause cause = damageEvent.getCause();
-        Entity entity = damageEvent.getEntity();
-        EntityType entityType = damageEvent.getEntityType();
-        HumanEntity humanEntity;
+        Entity defender = damageEvent.getEntity();
+        Entity attacker = entityDamage.getDamager();
 
-        baseAttack = (cause == DamageCause.ENTITY_ATTACK && entity instanceof HumanEntity);
-        sweepAttack = (cause == DamageCause.ENTITY_SWEEP_ATTACK && entity instanceof HumanEntity);
-        arrowAttack = (cause == DamageCause.PROJECTILE && entityType == EntityType.ARROW
-                && entity instanceof HumanEntity);
+        baseAttack = (cause == DamageCause.ENTITY_ATTACK);
+        sweepAttack = (cause == DamageCause.ENTITY_SWEEP_ATTACK);
+        arrowAttack = (cause == DamageCause.PROJECTILE);
 
-        if (baseAttack || sweepAttack || arrowAttack)
-            humanEntity = (HumanEntity) entity;
-        else
+        Bukkit.getLogger().info(cause.toString());
+        Bukkit.getLogger().info(defender.toString());
+        Bukkit.getLogger().info(attacker.toString());
+
+
+        baseDamage = damageEvent.getDamage();
+
+        if (!(defender instanceof LivingEntity && attacker instanceof LivingEntity)) {
+
+            Bukkit.getLogger().warning("ENTITY WAS NOT LIVING, DEFENDER TYPE: " + defender.getType());
+            Bukkit.getLogger().warning("ENTITY WAS NOT LIVING, ATTACKER TYPE: " + attacker.getType());
+            
             return;
+        }
 
-        calculateDamage(humanEntity);
+        // Cancel vanilla damage event.
+        damageEvent.setCancelled(true);
+
+        if (baseAttack || sweepAttack || arrowAttack) calculateDamage(defender, attacker);
+        else Bukkit.getLogger().warning("ENTITY DAMAGE WAS NOT CAUSED BY REGISTERED ATTACKS. ACTUAL DAMAGE CAUSE: " + cause);
 
     }
 
-    private void calculateDamage(HumanEntity humanEntity) {
+    private void calculateDamage(Entity defender, Entity attacker) {
 
-        PlayerInventory playerInventory = humanEntity.getInventory();
+        //Apply entities to living entities
 
-        ItemStack hand = playerInventory.getItemInMainHand();
-        ItemStack offHand = playerInventory.getItemInOffHand();
-        ItemStack helmet = playerInventory.getHelmet();
-        ItemStack chestplate = playerInventory.getChestplate();
-        ItemStack leggings = playerInventory.getLeggings();
-        ItemStack boots = playerInventory.getBoots();
+        LivingEntity defenderLiving = (LivingEntity) defender;
+        LivingEntity attackerLiving = (LivingEntity) attacker;
 
-        String classType;
-        String handWeaponType = hand.getItemMeta().getLocalizedName();
-        String offHandWeaponType = offHand.getItemMeta().getLocalizedName();
-        String helmetType = helmet.getItemMeta().getLocalizedName();
-        String chestplateType = chestplate.getItemMeta().getLocalizedName();
-        String leggingsType = leggings.getItemMeta().getLocalizedName();
-        String bootsType = boots.getItemMeta().getLocalizedName();
+        Config defenderClass;
+        Config attackerClass;
 
-        Config classConfig;
-        Config handWeaponConfig = getConfigType(handWeaponType);
-        Config offHandWeaponConfig = getConfigType(offHandWeaponType);
-        Config helmetConfig = getConfigType(helmetType);
-        Config chestplateConfig = getConfigType(chestplateType);
-        Config leggingsConfig = getConfigType(leggingsType);
-        Config bootsConfig = getConfigType(bootsType);
+        EntityEquipment defenderInventory = defenderLiving.getEquipment();
+        EntityEquipment attackerInventory = attackerLiving.getEquipment();
 
-        Integer classBaseArmor;
-        Integer classBaseHealth;
-        Integer handWeaponDamage = handWeaponConfig.weaponDamage;
-        Integer offHandWeaponDamage = offHandWeaponConfig.weaponDamage;
-        Integer helmetArmor = helmetConfig.armorValue;
-        Integer chestplateArmor = chestplateConfig.armorValue;
-        Integer leggingsArmor = leggingsConfig.armorValue;
-        Integer bootsArmor = bootsConfig.armorValue;
+        ItemStack[] armorContents = defenderInventory.getArmorContents();
 
-        Integer finalDamage;
+        ItemStack defenderHelmet = armorContents[0] != null ? armorContents[0] : new ItemStack(Material.AIR);
+        ItemStack defenderChestplate = armorContents[1] != null ? armorContents[1] : new ItemStack(Material.AIR);
+        ItemStack defenderLeggings = armorContents[2] != null ? armorContents[2] : new ItemStack(Material.AIR);
+        ItemStack defenderBoots = armorContents[3] != null ? armorContents[3] : new ItemStack(Material.AIR);
 
-        if (baseAttack && hand.getType() != Material.BOW) {
+        Bukkit.getLogger().info(defenderHelmet.toString());
+        Bukkit.getLogger().info(defenderChestplate.toString());
+        Bukkit.getLogger().info(defenderLeggings.toString());
+        Bukkit.getLogger().info(defenderBoots.toString());
 
-            finalDamage = handWeaponDamage;
+        Double defenderHelmetArmor = defenderHelmet.getType() != Material.AIR ? getConfigType(defenderHelmet.getItemMeta().getLocalizedName()).armorValue : 0.0;
+        Double defenderChestplateArmor = defenderChestplate.getType() != Material.AIR ? getConfigType(defenderChestplate.getItemMeta().getLocalizedName()).armorValue : 0.0;
+        Double defenderLeggingsArmor = defenderLeggings.getType() != Material.AIR ? getConfigType(defenderLeggings.getItemMeta().getLocalizedName()).armorValue : 0.0;
+        Double defenderBootsArmor = defenderBoots.getType() != Material.AIR ? getConfigType(defenderBoots.getItemMeta().getLocalizedName()).armorValue : 0.0;
 
-            //C
-            //
-            //Do base attack damage
+        ItemStack attackerWeapon;
+
+        if (arrowAttack) attackerWeapon = attackerInventory.getItemInMainHand().getType() == Material.BOW ? attackerInventory.getItemInMainHand() : attackerInventory.getItemInOffHand();
+        else attackerWeapon = attackerInventory.getItemInMainHand().getType() != Material.AIR ? attackerInventory.getItemInMainHand() : new ItemStack(Material.AIR);
+
+        Bukkit.getLogger().info(attackerWeapon.toString());
+
+        Double attackerWeaponDamage = attackerWeapon.getType() != Material.AIR ? getConfigType(attackerWeapon.getItemMeta().getLocalizedName()).weaponDamage : baseDamage;
+        Double totalArmor = defenderHelmetArmor + defenderChestplateArmor + defenderLeggingsArmor + defenderBootsArmor;
+        Double finalDamage;
+
+        if (baseAttack) {
+
+            if (attacker instanceof HumanEntity) finalDamage = totalArmor > 0 ? attackerWeaponDamage % totalArmor : attackerWeaponDamage;
+            else finalDamage = totalArmor > 0 ? baseDamage % totalArmor : baseDamage;
+
         }
 
-        if (sweepAttack) {
-            //do sweep attack damage
+        else if (sweepAttack) {
+
+            if (attacker instanceof HumanEntity) finalDamage = totalArmor > 0 ? attackerWeaponDamage % totalArmor : attackerWeaponDamage;
+            else finalDamage = totalArmor > 0 ? baseDamage % totalArmor : baseDamage;
+
         }
 
-        if (arrowAttack) {
-            //Do arrow damage
+        else if (arrowAttack) {
+
+            if (attacker instanceof HumanEntity) finalDamage = totalArmor > 0 ? attackerWeaponDamage % totalArmor : attackerWeaponDamage;
+            else finalDamage = totalArmor > 0 ? baseDamage % totalArmor : baseDamage;
+
         }
 
+        else return;
         
+        String log = "Final Damage: " + finalDamage + " Helmet Armor: " + defenderHelmetArmor + " Chestplate Armor: "
+                + defenderChestplateArmor + " Leggings Armor: " + defenderLeggingsArmor + " Boots Armor: "
+                + defenderBootsArmor + "Total Armor: " + totalArmor;
+
+        Bukkit.getLogger().info(log);
+
+        defenderLiving.damage(finalDamage);
 
     }
 
