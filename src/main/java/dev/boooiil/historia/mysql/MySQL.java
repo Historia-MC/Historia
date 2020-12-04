ce4dc38ad55b77485ba55c12ac58d2055064533f
boooiil <boooiil@outlook.com>
2020-12-04 17:30:32 -0500

    finalize methods

--- src/main/java/dev/boooiil/historia/mysql/MySQL.java
+++ src/main/java/dev/boooiil/historia/mysql/MySQL.java
@@ -3,49 +3,69 @@ package dev.boooiil.historia.mysql;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import dev.boooiil.historia.Config;
 
 public class MySQL {
-    
-    static Connection connection;
 
-    public static void intitate() {
+    private Connection connection;
 
-        final String database = new Config("MySQL").database;
-        final String username = new Config("MySQL").username;
-        final String password = new Config("MySQL").password;
-        final String ip = new Config("MySQL").ip;
-        final Integer port = new Config("MySQL").port;
-    
-        final String url = "jbdc:mysql://" + ip + ":" + port + "/" + database;
+    //Assign variables that we will use to connect to the database.
+    static final String DATABASE = new Config("MySQL").database;
+    static final String USERNAME = new Config("MySQL").username;
+    static final String PASSWORD = new Config("MySQL").password;
+    static final String IP = new Config("MySQL").ip;
+    static final Integer PORT = new Config("MySQL").port;
 
+    //Create a URL that we will use to connect to the MySQL database.
+    static final String URL = "jbdc:mysql://" + IP + ":" + PORT + "/" + DATABASE;
+
+    public void intitate() throws SQLException {
+
+        //Issue the statement that we will use to create the table if it does not exist.
         String createTable = "CREATE TABLE IF NOT EXISTS historia(UUID varchar(36), Username varchar(16), Class varchar(30), Level int(100), Experience int(10000))";
-        String fetchData = "SELECT WHERE * IN historia";
-        
-        try {
 
-            connection = DriverManager.getConnection(url, username, password);
+        //Try to prepare the statement and issue it to the database.
+        try (PreparedStatement create = connection.prepareStatement(createTable)) {
 
-            PreparedStatement create = connection.prepareStatement(createTable);
+            //Connect to the database and assign that connection to "connection".
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            
+            //Execute the prepared statement.
             create.executeQuery();
 
-        } catch (SQLException e) {
-            e.printStackTrace();
+            //Close out of the connection with the database.
+            connection.close();
+
         }
-        
     }
 
-    public static void doStatement() {
+    public void doStatement(String string) throws SQLException {
 
+        try (PreparedStatement statement = connection.prepareStatement(string)) {
+
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            statement.executeQuery();
+
+            connection.close();
+
+        }
     }
 
-    public static String getStatement() {
+    public ResultSet getStatement(String string) throws SQLException {
 
+        try (PreparedStatement statement = connection.prepareStatement(string)) {
+            
+            ResultSet results;
 
-        return "string";
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            results = statement.executeQuery();
+            connection.close();    
+
+            return results; 
+            
+        }
     }
-    
-
 }
