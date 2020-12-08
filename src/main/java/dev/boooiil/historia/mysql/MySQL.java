42118fb702f5b46485cffbe3616c29aa54e50d36
boooiil <boooiil@outlook.com>
2020-12-08 17:58:20 -0500

    Fixed typo on connection url

--- src/main/java/dev/boooiil/historia/mysql/MySQL.java
+++ src/main/java/dev/boooiil/historia/mysql/MySQL.java
@@ -12,7 +12,7 @@ import dev.boooiil.historia.Config;
 public class MySQL {
 
     private Connection connection;
-
+    
     private Statement statement;
 
     private static Config sql = new Config("MySQL");
@@ -25,26 +25,18 @@ public class MySQL {
     static final Integer PORT = sql.port;
 
     //Create a URL that we will use to connect to the MySQL database.
-    static final String URL = "jbdc:mysql://" + IP + ":" + PORT + "/" + DATABASE + "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+    static final String URL = "jdbc:mysql://" + IP + ":" + PORT + "/" + DATABASE + "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
 
     public void initiate() throws Exception {
 
         //Issue the statement that we will use to create the table if it does not exist.
         String createTable = "CREATE TABLE IF NOT EXISTS historia(UUID varchar(36), Username varchar(16), Class varchar(30), Level int(100), Experience int(10000))";
 
-        System.setProperty("jdbc.drivers", "com.mysql.cj.jdbc.Driver");
-        //Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
-
-        //Try to prepare the statement and issue it to the database.
-        //try (PreparedStatement create = connection.prepareStatement(createTable)) {
-
         //Connect to the database and assign that connection to "connection".
         connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
         statement = connection.createStatement();
         statement.executeQuery(createTable);
-            
-        //Execute the prepared statement.
-        //create.executeQuery();
 
         //Close out of the connection with the database.
         connection.close();
