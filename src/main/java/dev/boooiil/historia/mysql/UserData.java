40cd087439890d3cea3c3f7cd7a9d4871cee5f05
boooiil <boooiil@outlook.com>
2020-12-10 17:44:36 -0500

    remove unused imports

--- src/main/java/dev/boooiil/historia/mysql/UserData.java
+++ src/main/java/dev/boooiil/historia/mysql/UserData.java
@@ -1,12 +1,10 @@
 package dev.boooiil.historia.mysql;
 
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Date;
 import java.util.Map;
 import java.util.UUID;
 
-import javax.annotation.CheckForNull;
 
 import org.bukkit.entity.Player;
 
