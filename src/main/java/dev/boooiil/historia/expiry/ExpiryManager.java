9604eada4cedbb22818ae03edbce4e94d4266116
boooiil <boooiil@outlook.com>
2020-12-13 02:39:41 -0500

    deprecated

--- src/main/java/dev/boooiil/historia/expiry/ExpiryManager.java
+++ src/main/java/dev/boooiil/historia/expiry/ExpiryManager.java
@@ -10,6 +10,7 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
+@Deprecated
 public class ExpiryManager {
 
     /*
