f4aada576deae4b0bfbbeeabb689193d0b910f00
boooiil <boooiil@outlook.com>
2020-12-21 23:36:20 -0500

    tidy code

--- src/main/java/dev/boooiil/historia/alerts/BoatNotify.java
+++ src/main/java/dev/boooiil/historia/alerts/BoatNotify.java
@@ -12,26 +12,26 @@ import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.TextComponent;
 
 public class BoatNotify {
-    
+
+    private BoatNotify() { throw new IllegalStateException("Static utility class."); }
+
     public static void boatNotify(EquipmentSlot slot, Player player, Entity entity) {
-        if(slot.equals(EquipmentSlot.HAND)) {
+        if(slot.equals(EquipmentSlot.HAND) && !player.hasPermission("historia.boatplace")) {
             
-            if (!player.hasPermission("historia.boatplace")) {
+            String deathX = "" + Math.rint(entity.getLocation().getX());
+            String deathY = "" + Math.rint(entity.getLocation().getY());
+            String deathZ = "" + Math.rint(entity.getLocation().getZ());
+            String message = "§7[§9Alert§7] " + player.getName() + " interacted with a boat at §7[§9" + deathX + " " + deathY + " " + deathZ + "§7]";
     
-                String deathX = "" + Math.rint(entity.getLocation().getX());
-                String deathY = "" + Math.rint(entity.getLocation().getY());
-                String deathZ = "" + Math.rint(entity.getLocation().getZ());
+            Bukkit.getLogger().info(message);
+            for (Player online : Bukkit.getServer().getOnlinePlayers()) {
+                if (online.hasPermission("historia.alerts") || online.isOp()) {
     
-                System.out.println("§7[§9Alert§7] " + player.getName() + " interacted with a boat at §7[§9" + deathX + " " + deathY + " " + deathZ + "§7]");
-                for (Player online : Bukkit.getServer().getOnlinePlayers()) {
-                    if (online.hasPermission("historia.alerts") || online.isOp()) {
+                    TextComponent alert = new TextComponent("§7[§9Alert§7] " + player.getName() + " interacted with a boat at §7[§9" + deathX + " " + deathY + " " + deathZ + "§7]");
+                    alert.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/tp " + deathX + " " + deathY + " " + deathZ));
+                    alert.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("Teleport to " + deathX + ", " + deathY + ", " + deathZ).color(ChatColor.GREEN).create()));
     
-                        TextComponent message = new TextComponent("§7[§9Alert§7] " + player.getName() + " interacted with a boat at §7[§9" + deathX + " " + deathY + " " + deathZ + "§7]");
-                        message.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/tp " + deathX + " " + deathY + " " + deathZ));
-                        message.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("Teleport to " + deathX + ", " + deathY + ", " + deathZ).color(ChatColor.GREEN).create()));
-    
-                        online.spigot().sendMessage(message);
-                    }
+                    online.spigot().sendMessage(alert);
                 }
             }
         }
