334af8815368315a998f26262f6ae785c20a47b1
boooiil <boooiil@outlook.com>
2020-12-21 23:38:22 -0500

    addition of new methods

--- src/main/java/dev/boooiil/historia/mysql/MySQL.java
+++ src/main/java/dev/boooiil/historia/mysql/MySQL.java
@@ -5,8 +5,14 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
 
 import dev.boooiil.historia.Config;
 
@@ -81,5 +87,72 @@ public class MySQL {
             return map;
 
         }
+    }
+
+    public List<String> getUsernames() throws SQLException {
+
+        String string = "SELECT Username FROM historia";
+        List<String> answer = new ArrayList<>();
+
+        try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
+
+            // Connect to the database and assign that connection to "connection".
+
+            Statement statement = connection.createStatement();
+            ResultSet results = statement.executeQuery(string);
+
+            while (results.next()) {
+
+                answer.add(results.getString("Username"));
+
+            }
+
+            return answer;
+
+        }
+
+    } 
+
+    public UUID getUUID(String playerName) throws SQLException {
+
+        String string = "SELECT UUID FROM historia WHERE Username = '" + playerName + "'";
+
+        try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
+
+            // Connect to the database and assign that connection to "connection".
+
+            Statement statement = connection.createStatement();
+            ResultSet results = statement.executeQuery(string);
+
+            results.next();
+
+            return UUID.fromString(results.getString(1));
+
+        }
+
+    }
+
+    public List<UUID> getUUIDs() throws SQLException {
+
+        String string = "SELECT UUID FROM historia";
+        List<UUID> answer = new ArrayList<>();
+
+        try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
+
+            // Connect to the database and assign that connection to "connection".
+
+            Statement statement = connection.createStatement();
+            ResultSet results = statement.executeQuery(string);
+
+            while (results.next()) {
+
+                answer.add(UUID.fromString(results.getString("UUID")));
+
+            }
+
+            return answer;
+
+        }
+
     }
 }
