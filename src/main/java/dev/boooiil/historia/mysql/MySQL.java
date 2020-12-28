fdbabc660a9df0f70b3b752c9ab054bfa756812e
= <=>
2020-12-28 16:07:42 -0500

    remove unused dependencies

--- src/main/java/dev/boooiil/historia/mysql/MySQL.java
+++ src/main/java/dev/boooiil/historia/mysql/MySQL.java
@@ -6,14 +6,11 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
-
-import org.bukkit.Bukkit;
 
 import dev.boooiil.historia.Config;
 
