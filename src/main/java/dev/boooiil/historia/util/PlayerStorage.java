4dcd283f9d5ed208036fd3e57c09bf4d2def0972
boooiil <boooiil@outlook.com>
2022-07-10 23:10:37 -0400

    player persistence

---
+++ src/main/java/dev/boooiil/historia/util/PlayerStorage.java
@@ -0,0 +1,49 @@
+package dev.boooiil.historia.util;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+import dev.boooiil.historia.classes.HistoriaPlayer;
+
+public class PlayerStorage {
+
+    public static HashMap<UUID, HistoriaPlayer> players;
+
+    /**
+     * Add a player to our storage.
+     * 
+     * @param uuid - UUID of the player.
+     * @param historiaPlayer - {@link HistoriaPlayer} - Player object.
+     */
+    public static void addPlayer(UUID uuid, HistoriaPlayer historiaPlayer) {
+
+        players.put(uuid, historiaPlayer);
+
+    }
+
+    /**
+     * Get a player from our stored player list.
+     * 
+     * @param uuid - UUID of the player.
+     * @return {@link HistoriaPlayer} - The player you are requesting.
+     */
+    public static HistoriaPlayer getPlayer(UUID uuid) {
+
+        if (players.containsKey(uuid))
+            return players.get(uuid);
+
+        else
+            return null;
+
+    }
+
+    /**
+     * Save all player data to MySQL.
+     */
+    public void saveStates() {
+
+
+
+    }
+
+}
