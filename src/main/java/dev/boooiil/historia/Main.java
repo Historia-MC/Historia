73a1db6ebd624743d5bdcbcc287c5c21d59c4879
boooiil <boooiil@outlook.com>
2022-08-07 14:05:35 -0400

    rm unused import

--- src/main/java/dev/boooiil/historia/Main.java
+++ src/main/java/dev/boooiil/historia/Main.java
@@ -6,7 +6,6 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Server;
