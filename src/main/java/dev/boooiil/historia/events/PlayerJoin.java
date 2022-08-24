5fdfffcf4d1e479a9bb1daac26fd017686ada30f
boooiil <boooiil@outlook.com>
2022-08-23 23:54:44 -0400

    update presence on event

--- src/main/java/dev/boooiil/historia/events/PlayerJoin.java
+++ src/main/java/dev/boooiil/historia/events/PlayerJoin.java
@@ -1,10 +1,12 @@
 package dev.boooiil.historia.events;
 
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 import dev.boooiil.historia.classes.HistoriaPlayer;
+import dev.boooiil.historia.discord.HistoriaDiscord;
 import dev.boooiil.historia.util.PlayerStorage;
 
 public class PlayerJoin implements Listener {
@@ -13,6 +15,8 @@ public class PlayerJoin implements Listener {
     public void onPlayerJoin(PlayerJoinEvent event) {
 
         HistoriaPlayer historiaPlayer = new HistoriaPlayer(event.getPlayer().getUniqueId());
+
+        HistoriaDiscord.setActivity("Online: " + Bukkit.getOnlinePlayers().size() + "/" + Bukkit.getMaxPlayers());
 
         PlayerStorage.addPlayer(event.getPlayer().getUniqueId(), historiaPlayer);
 
--- src/main/java/dev/boooiil/historia/events/PlayerLeave.java
+++ src/main/java/dev/boooiil/historia/events/PlayerLeave.java
@@ -1,16 +1,20 @@
 package dev.boooiil.historia.events;
 
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerKickEvent;
 
 import dev.boooiil.historia.classes.HistoriaPlayer;
+import dev.boooiil.historia.discord.HistoriaDiscord;
 import dev.boooiil.historia.util.PlayerStorage;
 
 public class PlayerLeave implements Listener {
     
     @EventHandler
     public void onPlayerJoin(PlayerKickEvent event) {
+        
+        HistoriaDiscord.setActivity("Online: " + Bukkit.getOnlinePlayers().size() + "/" + Bukkit.getMaxPlayers());
 
         HistoriaPlayer historiaPlayer = PlayerStorage.getPlayer(event.getPlayer().getUniqueId(), false);
 
