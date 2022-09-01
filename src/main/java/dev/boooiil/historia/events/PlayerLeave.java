d81a27a91c1b9504c82db3b6434d4e477b41957b
boooiil <boooiil@outlook.com>
2022-08-31 22:22:46 -0400

    save logout in db

--- src/main/java/dev/boooiil/historia/events/PlayerLeave.java
+++ src/main/java/dev/boooiil/historia/events/PlayerLeave.java
@@ -3,21 +3,24 @@ package dev.boooiil.historia.events;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerKickEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 import dev.boooiil.historia.classes.HistoriaPlayer;
 import dev.boooiil.historia.discord.HistoriaDiscord;
+import dev.boooiil.historia.mysql.MySQLHandler;
 import dev.boooiil.historia.util.PlayerStorage;
 
 public class PlayerLeave implements Listener {
     
     @EventHandler
-    public void onPlayerJoin(PlayerKickEvent event) {
+    public void onPlayerJoin(PlayerQuitEvent event) {
         
         HistoriaPlayer historiaPlayer = PlayerStorage.getPlayer(event.getPlayer().getUniqueId(), false);
 
         historiaPlayer.saveCharacter();
         
+        MySQLHandler.setLogout(historiaPlayer.getUUID(), historiaPlayer.getLastLogin(), historiaPlayer.getPlaytime());
+
         PlayerStorage.removePlayer(event.getPlayer().getUniqueId());
         
         HistoriaDiscord.setActivity("Online: " + Bukkit.getOnlinePlayers().size() + "/" + Bukkit.getMaxPlayers());
