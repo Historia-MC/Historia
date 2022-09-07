62e8ef235bcf25141a056304d05f23de6c0f3296
boooiil <boooiil@outlook.com>
2022-09-06 23:39:43 -0400

    init

---
+++ src/main/java/dev/boooiil/historia/events/FurnaceSmeltFinish.java
@@ -0,0 +1,35 @@
+package dev.boooiil.historia.events;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.FurnaceSmeltEvent;
+
+import dev.boooiil.historia.configuration.IngotConfig;
+import dev.boooiil.historia.configuration.IngotConfig.Ingot;
+
+
+public class FurnaceSmeltFinish implements Listener {
+    
+    @EventHandler
+    public void onPlayerKill(FurnaceSmeltEvent event) {
+
+        //Not being fired, ??
+
+        String localizedName = event.getSource().getItemMeta().getLocalizedName();
+
+        if (IngotConfig.isValidIngot(localizedName)) {
+
+            Ingot ingot = IngotConfig.getIngot(localizedName);
+            
+            if (ingot.hasProgression()) {
+
+                event.setResult(ingot.getProgression().getItemStack());
+
+            }
+
+            
+        }
+
+    }
+
+}
