a48fe50baf85dd48ade5dcdc299ad7c2b7ee60ba
boooiil <boooiil@outlook.com>
2022-09-17 16:03:28 -0400

    finish

--- src/main/java/dev/boooiil/historia/configuration/ArmorConfig.java
+++ src/main/java/dev/boooiil/historia/configuration/ArmorConfig.java
@@ -1,6 +1,9 @@
 package dev.boooiil.historia.configuration;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Random;
 import java.util.Set;
 
 import org.bukkit.Material;
@@ -14,7 +17,8 @@ public class ArmorConfig {
 
     private static FileConfiguration configuration = FileGetter.get("amror.yml");
 
-    static final Set<String> armorSet = configuration.getKeys(false);
+    private static final Set<String> armorSet = configuration.getKeys(false);
+    private static final HashMap<String, Armor> armorMap = new HashMap<>();
 
     /**
      *
@@ -33,45 +37,127 @@ public class ArmorConfig {
     public static class Armor {
 
         private String type;
-        private String localizedName;
-        private String displayName;
-        private List<String> lore;
-        private int amount;
 
-        public String armorType;
+        private Integer weight;
 
-        public ItemStack armorItemStack;
+        private List<Double> defense;
+        private List<Integer> durability;
+        private List<String> recipeItems;
+        private List<String> recipeShape;
 
-        public List<String> armorCraftingShape;
-        public List<String> armorCreaftingRecipe;
+        private ItemStack itemStack;
 
-        public boolean validarmor;
-
-        public double armorValue;
+        private boolean valid;
 
         public Armor(String armorName) {
 
-            this.validarmor = validArmor(armorName);
+            this.valid = validArmor(armorName);
 
-            if (validarmor) {
-
-                String root = armorName;
+            if (this.valid) {
+     
                 String itemRoot = armorName + ".item";
 
                 this.type = configuration.getString(itemRoot + ".type");
-                this.localizedName = configuration.getString(itemRoot + ".loc-name");
-                this.displayName = configuration.getString(itemRoot + ".display-name");
-                this.lore = configuration.getStringList(itemRoot + ".lore");
-                this.amount = configuration.getInt(itemRoot + ".amount");
 
-                this.armorItemStack = Construct.itemStack(Material.getMaterial(type), amount, displayName, localizedName, lore);
+                this.weight = configuration.getInt(itemRoot + ".weight");
 
-                this.armorValue = configuration.getDouble(root + ".armor");
-                this.armorType = configuration.getString(root + ".type");
-                this.armorCraftingShape = configuration.getStringList(root + ".recipe-shape");
-                this.armorCreaftingRecipe = configuration.getStringList(root + ".recipe-items");
+                this.defense = configuration.getDoubleList(itemRoot + ".defense");
+                this.durability = configuration.getIntegerList(itemRoot + ".durability");
+                this.recipeItems = configuration.getStringList(itemRoot + ".recipe-items");
+                this.recipeShape = configuration.getStringList(itemRoot + ".recipe-shape");
+                
+                this.itemStack = Construct.itemStack(
+                    Material.getMaterial(configuration.getString(itemRoot + ".item.type")),
+                    configuration.getInt(itemRoot + ".item.amount"), 
+                    configuration.getString(itemRoot + ".item.display-name"), 
+                    configuration.getString(itemRoot + ".item.loc-name"), 
+                    configuration.getStringList(itemRoot + ".item.lore")
+                );
 
             }
+
+            else this.valid = false;
+
+        }
+
+        public String getType() {
+
+            return this.type;
+
+        }
+
+        public Integer getWeight() {
+
+            return this.weight;
+
+        }
+
+        public Double getRandomDefenseValue() {
+
+            Random random = new Random();
+            Double result = random.nextDouble() * (getMinDefenceValue() - getMaxDefenceValue()) + getMinDefenceValue();
+
+            return result;
+
+        }
+
+        public Integer getRandomDurabilityValue() {
+
+            Random random = new Random();
+            Integer result = random.nextInt() * (getMinDurabilityValue() - getMaxDurabilityValue()) + getMinDurabilityValue();
+
+            return result;
+
+        }
+
+        public List<String> getRecipeItems() {
+
+            return this.recipeItems;
+
+        }
+
+        public List<String> getRecipeShape() {
+
+            return this.recipeShape;
+
+        }
+
+        public Double getMinDefenceValue() {
+
+            return this.defense.get(0);
+
+        }
+
+        public Double getMaxDefenceValue() {
+
+            return this.defense.get(1);
+
+        }
+
+        public Integer getMinDurabilityValue() {
+
+            return this.durability.get(0);
+
+        }
+
+        
+        public Integer getMaxDurabilityValue() {
+
+            return this.durability.get(1);
+
+        }
+
+        public ItemStack getItemStack() {
+
+            return this.itemStack;
+
+        }
+        public Boolean isValidRecipe(List<String> inputItems, List<String> inputShape) {
+
+            boolean validItems = this.recipeItems.equals(inputItems);
+            boolean validShape = this.recipeShape.equals(inputShape);
+
+            return validItems && validShape;
 
         }
     }
@@ -85,6 +171,30 @@ public class ArmorConfig {
     public static boolean validArmor(String armorName) {
 
         return armorSet.contains(armorName);
+
+    }
+
+    // O(n^2) - cound be problematic as we increase the amount of armors
+    public static Armor getArmor(List<String> inputItems, List<String> inputShape) {
+
+        Armor armor = null;
+
+        for (Map.Entry<String, Armor> entry : armorMap.entrySet()) {
+
+            boolean armorValid = entry.getValue().isValidRecipe(inputItems, inputShape);
+
+            if (armorValid) { armor = entry.getValue(); break; }
+
+        }
+
+        return armor;
+        
+    }
+
+    public static Armor getArmor(String armorName) {
+
+        if (validArmor(armorName)) return armorMap.get(armorName);
+        else return null;
 
     }
 
