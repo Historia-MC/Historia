06cb50cd3c338b50149a28c02a1e60a783227721
boooiil <boooiil@outlook.com>
2023-03-27 02:31:50 -0400

    start of implementation

--- src/main/java/dev/boooiil/historia/classes/Item.java
+++ src/main/java/dev/boooiil/historia/classes/Item.java
@@ -1,10 +1,20 @@
 package dev.boooiil.historia.classes;
 
+import org.bukkit.configuration.file.FileConfiguration;
+
 public class Item {
 
     /**
      * INTENDED GENERIC TO EXTEND CLASSES THAT HOLD ITEMS.
      * TODO: IMPLEMENT
      */
-    
+
+    private FileConfiguration configuration;
+
+    protected Item() {
+
+
+
+    }
+
 }
