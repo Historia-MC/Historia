cf431d0c1d9e8d872800795156c22ca2eb5db4b1
boooiil <boooiil@outlook.com>
2023-03-27 12:50:26 -0400

    base class of created items, extends itemstack

--- src/main/java/dev/boooiil/historia/classes/Item.java
+++ src/main/java/dev/boooiil/historia/classes/Item.java
@@ -1,20 +1,49 @@
 package dev.boooiil.historia.classes;
 
+import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.inventory.ItemStack;
 
-public class Item {
+public class Item extends ItemStack {
 
     /**
      * INTENDED GENERIC TO EXTEND CLASSES THAT HOLD ITEMS.
      * TODO: IMPLEMENT
      */
 
-    private FileConfiguration configuration;
+    protected FileConfiguration configuration;
+    protected boolean valid;
 
     protected Item() {
 
-
+        initItemStack(new ItemStack(Material.AIR));
 
     }
 
+    protected void initItemStack(ItemStack itemStack) {
+
+        this.setAmount(itemStack.getAmount());
+        this.setData(itemStack.getData());
+        this.setItemMeta(itemStack.getItemMeta());
+        this.setType(itemStack.getType());
+
+    }
+
+    /**
+     * It returns the item stack
+     * 
+     * @return The itemStack variable.
+     */
+    @Deprecated
+    public ItemStack getItemStack() {
+
+        return this;
+
+    }
+
+    public boolean isValid() {
+
+        return this.valid;
+
+    }
 }
