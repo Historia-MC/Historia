bd547c1aa55436599cae5da4d1c865e332888081
boooiil <boooiil@outlook.com>
2023-03-31 04:31:59 -0400

    testing

--- src/test/java/dev/boooiil/historia/Driver.java
+++ src/test/java/dev/boooiil/historia/Driver.java
@@ -3,41 +3,41 @@ package dev.boooiil.historia;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import be.seeseemelk.mockbukkit.MockBukkit;
 import be.seeseemelk.mockbukkit.ServerMock;
-import dev.boooiil.historia.classes.HistoriaPlayer;
+import dev.boooiil.historia.classes.historia.HistoriaPlayer;
+import dev.boooiil.historia.util.Logging;
 import dev.boooiil.historia.util.PlayerStorage;
 
 public class Driver {
     
     private ServerMock server;
     private HistoriaPlugin plugin;
-
+    private PluginDescriptionFile descriptionFile;
     private HistoriaPlayer historiaPlayer;
  
 
     @Before
     public void setUp() {
 
-        System.out.println("SETTING UP TESTS");
+        Logging.infoToConsole("SETTING UP TESTS");
+        
+        descriptionFile = new PluginDescriptionFile("Historia", "6.0", "dev.boooiil.historia.HistoriaPlugin"); 
+
+        Logging.infoToConsole("DESC: " + descriptionFile);
 
         server = MockBukkit.mock();
-        plugin = MockBukkit.load(HistoriaPlugin.class);
-        
+        plugin = MockBukkit.loadWith(dev.boooiil.historia.HistoriaPlugin.class, descriptionFile);
+
         Player player = server.addPlayer();
         historiaPlayer = new HistoriaPlayer(player.getUniqueId(), server);
 
-        PlayerStorage.addPlayer(
-            player.getUniqueId(),
-            historiaPlayer
-        );
-
-        PlayerStorage.getPlayer(player.getUniqueId(), false).saveCharacter();
-        server.addPlayer();
+        PlayerStorage.addPlayer(player.getUniqueId(), historiaPlayer);
 
     }
 
@@ -45,7 +45,7 @@ public class Driver {
     public void assertion_PlayerStorage() {
 
         assertEquals(1, PlayerStorage.players.size());
-        
+
     }
 
     @After
---
+++ src/test/java/dev/boooiil/historia/classes/MockHistoriaPlayer.java
@@ -0,0 +1,17 @@
+package dev.boooiil.historia.classes;
+
+import java.util.UUID;
+
+import org.bukkit.Server;
+
+import dev.boooiil.historia.classes.historia.HistoriaPlayer;
+
+public class MockHistoriaPlayer extends HistoriaPlayer {
+
+    MockHistoriaPlayer(UUID uuid, Server server) {
+
+        
+
+    }
+    
+}
