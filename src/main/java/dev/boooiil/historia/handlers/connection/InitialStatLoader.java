030acc9da8741da3a89400d657348b4af394b7ce
boooiil <boooiil@outlook.com>
2023-07-12 12:07:03 -0400

    set health, experience, and speed levels

--- src/main/java/dev/boooiil/historia/handlers/connection/InitialStatLoader.java
+++ src/main/java/dev/boooiil/historia/handlers/connection/InitialStatLoader.java
@@ -1,5 +1,7 @@
 package dev.boooiil.historia.handlers.connection;
 
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeInstance;
 import org.bukkit.entity.Player;
 
 import dev.boooiil.historia.classes.historia.user.HistoriaPlayer;
@@ -20,10 +22,24 @@ public class InitialStatLoader {
     public void apply() {
 
         //TODO: Need to make sure that the player is not losing health or food each time they join if base health > 20
-        player.setHealth(historiaPlayer.getProficiency().getStats().getBaseHealth() - (20 - player.getHealth()));
-        player.setTotalExperience(historiaPlayer.getLevel());
-        player.setWalkSpeed(historiaPlayer.getProficiency().getStats().getBaseSpeed());
-        player.setFoodLevel(historiaPlayer.getProficiency().getStats().getBaseFood() - (20 - player.getFoodLevel()));
+        //base health scale: getHealth() / getMaxHealth() * getHealthScale().
+
+        AttributeInstance healthAttribute = player.getAttribute(Attribute.GENERIC_MAX_HEALTH);
+        
+        if (healthAttribute.getBaseValue() != historiaPlayer.getProficiency().getStats().getBaseHealth()) {
+
+            healthAttribute.setBaseValue(historiaPlayer.getProficiency().getStats().getBaseHealth());
+            player.setHealth(historiaPlayer.getProficiency().getStats().getBaseHealth() * (player.getHealth()/20));
+
+        }
+
+        player.setWalkSpeed(0.2f * historiaPlayer.getProficiency().getStats().getBaseSpeed());
+
+        player.setLevel(historiaPlayer.getLevel());
+        
+        //just dont bother with this, update food status on consume event
+        //player.setFoodLevel(historiaPlayer.getProficiency().getStats().getBaseFood() * (player.getFoodLevel()/20));
+
 
     }
     
