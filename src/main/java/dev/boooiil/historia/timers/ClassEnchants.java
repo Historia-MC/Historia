c3de970616a502de8f49bc69ecde74f76416fdd8
boooiil <boooiil@outlook.com>
2023-07-12 13:31:32 -0400

    add missing methods

--- src/main/java/dev/boooiil/historia/timers/ClassEnchants.java
+++ src/main/java/dev/boooiil/historia/timers/ClassEnchants.java
@@ -1,5 +1,20 @@
 package dev.boooiil.historia.timers;
 
-public class ClassEnchants {
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class ClassEnchants extends BukkitRunnable {
+
+    private final JavaPlugin plugin;
+
+    public ClassEnchants(JavaPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'run'");
+    }
     
 }
--- src/main/java/dev/boooiil/historia/timers/Expiry.java
+++ src/main/java/dev/boooiil/historia/timers/Expiry.java
@@ -1,5 +1,20 @@
 package dev.boooiil.historia.timers;
 
-public class Expiry {
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class Expiry extends BukkitRunnable {
+
+    private final JavaPlugin plugin;
+
+    public Expiry(JavaPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'run'");
+    }
     
 }
