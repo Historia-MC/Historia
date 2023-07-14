0422330387beb992a6b8747fbe828b779654e452
boooiil <boooiil@outlook.com>
2023-07-14 11:26:47 -0400

    apply class enchant

--- src/main/java/dev/boooiil/historia/runnable/ClassEnchants.java
+++ src/main/java/dev/boooiil/historia/runnable/ClassEnchants.java
@@ -1,20 +1,68 @@
 package dev.boooiil.historia.runnable;
 
-import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.Inventory;
 
-public class ClassEnchants extends BukkitRunnable {
+import dev.boooiil.historia.classes.historia.user.HistoriaPlayer;
 
-    private final JavaPlugin plugin;
+public class ClassEnchants {
 
-    public ClassEnchants(JavaPlugin plugin) {
-        this.plugin = plugin;
+    private HistoriaPlayer historiaPlayer;
+    private Inventory inventory;
+
+    public ClassEnchants(HistoriaPlayer historiaPlayer, Inventory inventory) {
+        this.historiaPlayer = historiaPlayer;
+        this.inventory = inventory;
     }
 
-    @Override
-    public void run() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'run'");
+    public void doApplyEnchants() {
+
+        inventory.iterator().forEachRemaining(item -> {
+
+            if (item.getType().toString().contains("PICKAXE")) {
+
+                if (historiaPlayer.getProficiency().getSkills().hasEfficiencyPickaxe()) {
+
+                    if (!item.getItemMeta().hasEnchant(Enchantment.DIG_SPEED)) {
+
+                        item.getItemMeta().addEnchant(Enchantment.DIG_SPEED, 1, true);
+
+                    }
+
+                } else {
+
+                    if (item.getItemMeta().hasEnchant(Enchantment.DIG_SPEED)) {
+
+                        item.getItemMeta().removeEnchant(Enchantment.DIG_SPEED);
+
+                    }
+
+                }
+
+            }
+
+            if (item.getType().toString().contains("AXE")) {
+                if (historiaPlayer.getProficiency().getSkills().hasEfficiencyAxe()) {
+
+                    if (!item.getItemMeta().hasEnchant(Enchantment.DIG_SPEED)) {
+
+                        item.getItemMeta().addEnchant(Enchantment.DIG_SPEED, 1, true);
+
+                    }
+
+                } else {
+
+                    if (item.getItemMeta().hasEnchant(Enchantment.DIG_SPEED)) {
+
+                        item.getItemMeta().removeEnchant(Enchantment.DIG_SPEED);
+
+                    }
+
+                }
+            }
+
+        });
+
     }
-    
+
 }
