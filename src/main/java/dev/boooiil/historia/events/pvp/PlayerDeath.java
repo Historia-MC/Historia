7b9940aaec983d526f8cbb9534b9ca7ee48ff5f9
boooiil <boooiil@outlook.com>
2023-07-14 10:54:51 -0400

    class to handle player death events

---
+++ src/main/java/dev/boooiil/historia/events/pvp/PlayerDeath.java
@@ -0,0 +1,25 @@
+package dev.boooiil.historia.events.pvp;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+import dev.boooiil.historia.classes.historia.user.HistoriaPlayer;
+import dev.boooiil.historia.handlers.pvp.PlayerKilled;
+import dev.boooiil.historia.util.PlayerStorage;
+
+public class PlayerDeath implements Listener {
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+
+        Player deadEntity = event.getEntity();
+        HistoriaPlayer historiaKiller = PlayerStorage.getPlayer(deadEntity.getKiller().getUniqueId(), false);
+
+        PlayerKilled playerKilled = new PlayerKilled(event, historiaKiller);
+        playerKilled.doDeath();
+
+    }
+
+}
