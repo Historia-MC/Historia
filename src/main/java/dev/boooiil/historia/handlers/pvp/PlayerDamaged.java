c40f9a42aed2caae8de8ad726d8b54c83e39f611
boooiil <boooiil@outlook.com>
2023-07-14 10:55:57 -0400

    class to handle player damage events

---
+++ src/main/java/dev/boooiil/historia/handlers/pvp/PlayerDamaged.java
@@ -0,0 +1,99 @@
+package dev.boooiil.historia.handlers.pvp;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.inventory.ItemStack;
+
+import dev.boooiil.historia.classes.historia.user.HistoriaPlayer;
+import dev.boooiil.historia.classes.items.craftable.Weapon;
+import dev.boooiil.historia.configuration.Config;
+import dev.boooiil.historia.util.Logging;
+import dev.boooiil.historia.util.PlayerStorage;
+
+public class PlayerDamaged {
+
+    private Player attacker;
+    private Player defender;
+    private EntityDamageByEntityEvent event;
+    private HistoriaPlayer historiaAttacker;
+    private HistoriaPlayer historiaDefender;
+
+    public PlayerDamaged(EntityDamageByEntityEvent event) {
+
+        this.event = event;
+        this.attacker = (Player) event.getDamager();
+        this.defender = (Player) event.getEntity();
+        this.historiaAttacker = PlayerStorage.getPlayer(attacker.getUniqueId(), false);
+        this.historiaDefender = PlayerStorage.getPlayer(defender.getUniqueId(), false);
+
+    }
+
+    // calculate and apply base damage values to the event
+    public void doAttack() {
+
+        if (event.isCancelled()) {
+            return;
+        }
+
+        ItemStack weapon = attacker.getInventory().getItemInMainHand();
+
+        // if it is a valid weapon from the config
+        if (Config.getWeaponConfig().isValid(weapon.getItemMeta().getLocalizedName())) {
+
+            Weapon historiaWeapon = Config.getWeaponConfig().getObject(weapon.getItemMeta().getLocalizedName());
+
+            // if the player is proficient with the weapon
+            if (historiaAttacker.getProficiency().getStats().getWeaponProficiency()
+                    .contains(historiaWeapon.getWeightClass())) {
+
+                // update the weapon stats from the given lore
+                historiaWeapon.updateWeaponStats(weapon.getItemMeta().getLore());
+
+                // reduce damage based on the sword (generic attack) proficiency
+                double damage = historiaWeapon.getDamage()
+                        * historiaAttacker.getProficiency().getStats().getBaseSwordProficiency();
+
+                Logging.infoToPlayer("You hit " + defender.getCustomName() + " for " + damage + " damage!",
+                        attacker.getUniqueId());
+                Logging.infoToPlayer(attacker.getCustomName() + " hit you for " + damage + " damage!",
+                        defender.getUniqueId());
+
+                event.setDamage(damage);
+
+            } else {
+
+                Logging.infoToPlayer("You are not proficient with this weapon!", attacker.getUniqueId());
+                Logging.infoToPlayer(attacker.getCustomName() + " is not proficient with their weapon and misses!",
+                        defender.getUniqueId());
+
+                event.setCancelled(true);
+                return;
+
+            }
+        }
+
+    }
+
+    public void doDefend() {
+
+        if (event.isCancelled()) {
+            return;
+        }
+
+        float evasionRoll = Math.round(Math.random() * 100) / 100;
+
+        if (evasionRoll < historiaDefender.getProficiency().getStats().getBaseEvasion()) {
+
+            Logging.infoToPlayer(defender.getCustomName() + " has evaded the attack!",
+                    attacker.getUniqueId());
+            Logging.infoToPlayer("You evaded an attack from " + attacker.getCustomName() + "!",
+                    defender.getUniqueId());
+
+            event.setCancelled(true);
+            return;
+
+        }
+
+    }
+
+}
