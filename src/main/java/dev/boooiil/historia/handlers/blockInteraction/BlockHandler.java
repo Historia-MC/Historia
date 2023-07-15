1f4635561cf9262f9b43e77b9a538b516f220221
boooiil <boooiil@outlook.com>
2023-07-15 01:50:03 -0400

    check crop safety

--- src/main/java/dev/boooiil/historia/handlers/blockInteraction/BlockHandler.java
+++ src/main/java/dev/boooiil/historia/handlers/blockInteraction/BlockHandler.java
@@ -28,28 +28,35 @@ public class BlockHandler extends BaseBlockHandler {
 
     public void doBreak() {
 
+        CropHandler.safetyCheckBlockBrokenHoldsCrop(breakEvent.getBlock());
+
         if (oreConfig.isValidOre(breakEvent.getBlock().getType().toString())) {
 
             Float doulbeDropChance = 0.05f;
             Float dropChanceRoll = (float) Math.round((Math.random() * 100)) / 100;
 
-            boolean didDouble = historiaPlayer.getProficiency().getSkills().hasChanceExtraOre() ? dropChanceRoll <= doulbeDropChance : false;
+            boolean didDouble = historiaPlayer.getProficiency().getSkills().hasChanceExtraOre()
+                    ? dropChanceRoll <= doulbeDropChance
+                    : false;
 
-            OreDrop drop = oreConfig.getDropFromChance(breakEvent.getBlock().getType().toString(), historiaPlayer.getProficiency().getName());
+            OreDrop drop = oreConfig.getDropFromChance(breakEvent.getBlock().getType().toString(),
+                    historiaPlayer.getProficiency().getName());
 
             if (drop != null) {
 
                 breakEvent.setCancelled(true);
-                breakEvent.getBlock().getWorld().dropItemNaturally(breakEvent.getBlock().getLocation(), drop.getItemStack());
+                breakEvent.getBlock().getWorld().dropItemNaturally(breakEvent.getBlock().getLocation(),
+                        drop.getItemStack());
                 if (didDouble) {
-                    breakEvent.getBlock().getWorld().dropItemNaturally(breakEvent.getBlock().getLocation(), drop.getItemStack());
+                    breakEvent.getBlock().getWorld().dropItemNaturally(breakEvent.getBlock().getLocation(),
+                            drop.getItemStack());
                     Logging.infoToPlayer("You have doubled your ore drop!", historiaPlayer.getUUID());
-                
+
                 }
-                
+
                 breakEvent.getBlock().setType(Material.AIR);
 
-                //TODO: set experience for player
+                // TODO: set experience for player
 
             }
 
@@ -67,8 +74,10 @@ public class BlockHandler extends BaseBlockHandler {
                 if (didDouble) {
 
                     breakEvent.setCancelled(true);
-                    breakEvent.getBlock().getWorld().dropItemNaturally(breakEvent.getBlock().getLocation(), breakEvent.getBlock().getDrops().iterator().next());
-                    breakEvent.getBlock().getWorld().dropItemNaturally(breakEvent.getBlock().getLocation(), breakEvent.getBlock().getDrops().iterator().next());
+                    breakEvent.getBlock().getWorld().dropItemNaturally(breakEvent.getBlock().getLocation(),
+                            breakEvent.getBlock().getDrops().iterator().next());
+                    breakEvent.getBlock().getWorld().dropItemNaturally(breakEvent.getBlock().getLocation(),
+                            breakEvent.getBlock().getDrops().iterator().next());
                     Logging.infoToPlayer("You have doubled your log drop!", historiaPlayer.getUUID());
                     historiaPlayer.increaseExperience();
 
@@ -85,15 +94,18 @@ public class BlockHandler extends BaseBlockHandler {
                 breakEvent.setCancelled(true);
                 Logging.infoToPlayer("You have no idea what to do with this thing!", historiaPlayer.getUUID());
 
-
             }
 
         }
+
+        else {
+        }
+
     }
 
     public void doPlace() {
 
-        //TODO: check for ores
+        // TODO: check for ores
 
         if (historiaPlayer.getProficiency().getSkills().hasChanceNoConsumeBlock()) {
 
@@ -105,5 +117,4 @@ public class BlockHandler extends BaseBlockHandler {
 
     }
 
-    
 }
