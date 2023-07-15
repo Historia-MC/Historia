71665d1da6277e3cdf48deb0ce98cab0f2d99625
boooiil <boooiil@outlook.com>
2023-07-15 05:09:08 -0400

    check crafting items

--- src/main/java/dev/boooiil/historia/events/crafting/PlayerCraft.java
+++ src/main/java/dev/boooiil/historia/events/crafting/PlayerCraft.java
@@ -4,9 +4,11 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.PrepareItemCraftEvent;
+import org.bukkit.inventory.ItemStack;
 
-import dev.boooiil.historia.handlers.crafting.CustomCraftingManager;
-import dev.boooiil.historia.util.Logging;
+import dev.boooiil.historia.handlers.crafting.ArmorAndWeaponManager;
+import dev.boooiil.historia.handlers.crafting.CraftingCustomItemManager;
+import dev.boooiil.historia.handlers.crafting.TableInspector;
 
 public class PlayerCraft implements Listener {
 
@@ -14,12 +16,19 @@ public class PlayerCraft implements Listener {
     public void onCraftItem(PrepareItemCraftEvent event) {
 
         Player player = (Player) event.getViewers().get(0);
+        TableInspector inspector = new TableInspector(event.getInventory().getContents());
+        ItemStack resultItem;
 
-        Logging.infoToConsole(player.getName());
+        ArmorAndWeaponManager ccm = new ArmorAndWeaponManager(inspector);
+        CraftingCustomItemManager ccim = new CraftingCustomItemManager(inspector);
 
-        CustomCraftingManager ccm = new CustomCraftingManager(event.getInventory());
 
-        event.getInventory().setResult(ccm.getResult());
+        if (ccm.getResult() != null) resultItem = ccm.getResult();
+        else if (ccim.getResult() != null) resultItem = ccim.getResult();
+        else resultItem = event.getRecipe().getResult();
+
+
+        event.getInventory().setResult(resultItem);
 
     }
 
