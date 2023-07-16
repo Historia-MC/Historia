7a1d70eab2eb0f70c40547c7aeb225df383c7c58
boooiil <boooiil@outlook.com>
2023-07-16 07:16:07 -0400

    documentation, check if proficiency can use/craft armor/weapon

--- src/main/java/dev/boooiil/historia/classes/historia/proficiency/Proficiency.java
+++ src/main/java/dev/boooiil/historia/classes/historia/proficiency/Proficiency.java
@@ -1,19 +1,40 @@
 package dev.boooiil.historia.classes.historia.proficiency;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.bukkit.configuration.file.FileConfiguration;
 
 import dev.boooiil.historia.classes.enums.FileMap.ResourceKeys;
 import dev.boooiil.historia.util.FileGetter;
 
+/**
+ * This class represents a proficiency that a character can have. It contains
+ * information about the proficiency's name, stats, and skills.
+ */
 public class Proficiency {
-    
+
+    /**
+     * The name of the proficiency.
+     */
     private String name;
 
+    /**
+     * The stats associated with the proficiency.
+     */
     private Stats stats;
+
+    /**
+     * The skills associated with the proficiency.
+     */
     private Skills skills;
 
+    /**
+     * Constructs a new proficiency with the given name.
+     * 
+     * @param proficiencyName the name of the proficiency
+     */
     public Proficiency(String proficiencyName) {
-
         FileConfiguration config = FileGetter.get(ResourceKeys.PROFICIENCY);
 
         this.name = proficiencyName;
@@ -21,30 +42,287 @@ public class Proficiency {
         this.skills = new Skills(config, proficiencyName + ".skills");
     }
 
+    /**
+     * Returns the name of the proficiency.
+     * 
+     * @return the name of the proficiency
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the name of the proficiency.
+     * 
+     * @param proficiencyName the new name of the proficiency
+     */
     public void setName(String proficiencyName) {
         this.name = proficiencyName;
     }
 
+    /**
+     * Returns the stats associated with the proficiency.
+     * 
+     * @return the stats associated with the proficiency
+     */
     public Stats getStats() {
         return stats;
     }
 
+    /**
+     * Sets the stats associated with the proficiency.
+     * 
+     * @param stats the new stats associated with the proficiency
+     */
     public void setStats(Stats stats) {
         this.stats = stats;
     }
 
+    /**
+     * Returns the skills associated with the proficiency.
+     * 
+     * @return the skills associated with the proficiency
+     */
     public Skills getSkills() {
         return skills;
     }
 
+    /**
+     * Sets the skills associated with the proficiency.
+     * 
+     * @param skills the new skills associated with the proficiency
+     */
     public void setSkills(Skills skills) {
         this.skills = skills;
     }
 
+    /**
+     * Returns a boolean value indicating whether the character can use light armor
+     * or not.
+     * 
+     * @return true if the character can use light armor, false otherwise
+     */
+    public boolean canUseLightArmor() {
+        return getStats().getArmorProficiency().contains("Light");
+    }
+
+    /**
+     * Returns a boolean value indicating whether the character can use medium armor
+     * or not.
+     * 
+     * @return true if the character can use medium armor, false otherwise
+     */
+    public boolean canUseMediumArmor() {
+        return getStats().getArmorProficiency().contains("Medium");
+    }
+
+    /**
+     * Returns a boolean value indicating whether the character can use heavy armor
+     * or not.
+     * 
+     * @return true if the character can use heavy armor, false otherwise
+     */
+    public boolean canUseHeavyArmor() {
+        return getStats().getArmorProficiency().contains("Heavy");
+    }
+
+    /**
+     * Returns a boolean value indicating whether the character can use light
+     * weapons or not.
+     * 
+     * @return true if the character can use light weapons, false otherwise
+     */
+    public boolean canUseLightWeapon() {
+        return getStats().getWeaponProficiency().contains("Light");
+    }
+
+    /**
+     * Returns a boolean value indicating whether the character can use medium
+     * weapons or not.
+     * 
+     * @return true if the character can use medium weapons, false otherwise
+     */
+    public boolean canUseMediumWeapon() {
+        return getStats().getWeaponProficiency().contains("Medium");
+    }
+
+    /**
+     * This function returns a boolean value indicating whether the character can
+     * use heavy weapons or not.
+     * 
+     * @return true if the character can use heavy weapons, false otherwise.
+     */
+    public boolean canUseHeavyWeapon() {
+        return getStats().getWeaponProficiency().contains("Heavy");
+    }
+
+    /**
+     * This function returns a boolean value indicating whether the character can
+     * use bows or not.
+     * 
+     * @return true if the character can use bows, false otherwise.
+     */
+    public boolean canUseBow() {
+        return getStats().getWeaponProficiency().contains("Bow");
+    }
+
+    /**
+     * This function returns a boolean value indicating whether the character can
+     * use crossbows or not.
+     * 
+     * @return true if the character can use crossbows, false otherwise.
+     */
+    public boolean canUseCrossbow() {
+        return getStats().getWeaponProficiency().contains("Crossbow");
+    }
+
+    /**
+     * This function returns a boolean value indicating whether the character can
+     * use a weapon with the given localizedName or not.
+     * 
+     * @param localizedName the name of the weapon to check proficiency for.
+     * @return true if the character can use the weapon, false otherwise.
+     */
+    public boolean canUseWeapon(String localizedName) {
+
+        Pattern tier = Pattern.compile("Light|Medium|Heavy|Trident|Bow|Crossbow");
+        Matcher matcher = tier.matcher(localizedName);
+
+        if (matcher.matches()) {
+
+            return getStats().getWeaponProficiency().contains(localizedName);
+
+        }
+
+        else {
+
+            return false;
+
+        }
+
+    }
+
+    /**
+     * This function returns a boolean value indicating whether the character can
+     * use an armor with the given localizedName or not.
+     * 
+     * @param localizedName the name of the armor to check proficiency for.
+     * @return true if the character can use the armor, false otherwise.
+     */
+    public boolean canUseArmor(String localizedName) {
+
+        Pattern tier = Pattern.compile("Light|Medium|Heavy");
+        Matcher matcher = tier.matcher(localizedName);
+
+        if (matcher.matches()) {
+
+            return getStats().getArmorProficiency().contains(localizedName);
+
+        }
+
+        else {
+
+            return false;
+
+        }
+
+    }
+
+    /**
+     * This function returns a boolean value indicating whether the character can
+     * craft a weapon with the given localizedName or not.
+     * 
+     * @param localizedName the name of the weapon to check crafting proficiency
+     *                      for.
+     * @return true if the character can craft the weapon, false otherwise.
+     */
+    public boolean canCraftWeapon(String localizedName) {
+
+        Pattern tier = Pattern.compile("Light|Medium|Heavy|Trident|Bow|Crossbow");
+        Matcher matcher = tier.matcher(localizedName);
+
+        if (matcher.matches()) {
+
+            String matched = matcher.group(0);
+
+            if (getSkills().canCraftHighTier()) {
+                return true;
+            }
+
+            else if (matched.equals("Light")) {
+                return true;
+            }
+
+            else if (matched.equals("Trident") && canUseWeapon("Trident")) {
+                return true;
+            }
+
+            else if (matched.equals("Bow") && canUseWeapon("Bow")) {
+                return true;
+            }
+
+            else if (matched.equals("Crossbow") && canUseWeapon("Crossbow")) {
+                return true;
+            }
+
+            else {
+                return false;
+            }
+
+        }
+
+        else {
+
+            return false;
+
+        }
+
+    }
+
+    /**
+     * This function returns a boolean value indicating whether the character can
+     * craft an armor with the given localizedName or not.
+     * 
+     * @param localizedName the name of the armor to check crafting proficiency for.
+     * @return true if the character can craft the armor, false otherwise.
+     */
+    public boolean canCraftArmor(String localizedName) {
+
+        Pattern tier = Pattern.compile("Light|Medium|Heavy");
+        Matcher matcher = tier.matcher(localizedName);
+
+        if (matcher.matches()) {
+
+            String matched = matcher.group(0);
+
+            if (getSkills().canCraftHighTier()) {
+                return true;
+            }
+
+            else if (matched.equals("Light")) {
+                return true;
+            }
+
+            else {
+                return false;
+            }
+
+        }
+
+        else {
+
+            return false;
+
+        }
+
+    }
+
+    /**
+     * Returns a string representation of the Proficiency object.
+     * 
+     * @return a string containing the name, stats, and skills of the Proficiency
+     *         object.
+     */
     public String toString() {
 
         String output = "*** PROFICIENCY ***\n";
