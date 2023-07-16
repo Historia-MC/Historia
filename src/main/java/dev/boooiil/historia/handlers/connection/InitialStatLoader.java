5edd45b755d23612cd665a847b6f096912615325
boooiil <boooiil@outlook.com>
2023-07-16 04:20:47 -0400

    scale adjusted health on class selection

--- src/main/java/dev/boooiil/historia/handlers/connection/InitialStatLoader.java
+++ src/main/java/dev/boooiil/historia/handlers/connection/InitialStatLoader.java
@@ -24,12 +24,13 @@ public class InitialStatLoader {
         //TODO: Need to make sure that the player is not losing health or food each time they join if base health > 20
         //base health scale: getHealth() / getMaxHealth() * getHealthScale().
 
+        double previousHealth = player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getDefaultValue();
         AttributeInstance healthAttribute = player.getAttribute(Attribute.GENERIC_MAX_HEALTH);
         
         if (healthAttribute.getBaseValue() != historiaPlayer.getProficiency().getStats().getBaseHealth()) {
 
             healthAttribute.setBaseValue(historiaPlayer.getProficiency().getStats().getBaseHealth());
-            player.setHealth(historiaPlayer.getProficiency().getStats().getBaseHealth() * (player.getHealth()/20));
+            player.setHealth(historiaPlayer.getProficiency().getStats().getBaseHealth() * (player.getHealth() / previousHealth));
 
         }
 
