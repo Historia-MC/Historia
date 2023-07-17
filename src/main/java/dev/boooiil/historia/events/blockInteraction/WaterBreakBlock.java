6e5ac5d3a313ee1b2d39e5bd23098f9b9d4b4dc1
boooiil <boooiil@outlook.com>
2023-07-17 06:02:31 -0400

    check if water breaks block

---
+++ src/main/java/dev/boooiil/historia/events/blockInteraction/WaterBreakBlock.java
@@ -0,0 +1,25 @@
+package dev.boooiil.historia.events.blockInteraction;
+
+import org.bukkit.block.Block;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockFromToEvent;
+
+import dev.boooiil.historia.configuration.ConfigurationLoader;
+import dev.boooiil.historia.configuration.specific.CropConfig;
+
+public class WaterBreakBlock implements Listener {
+
+    @EventHandler
+    public void onWaterBreakBlock(BlockFromToEvent event) {
+
+        Block toBlock = event.getToBlock();
+        CropConfig cropConfig = ConfigurationLoader.getCropConfig();
+
+        if (cropConfig.isCrop(toBlock.getType())) {
+            event.setCancelled(true);;
+        }
+        
+    }
+    
+}
