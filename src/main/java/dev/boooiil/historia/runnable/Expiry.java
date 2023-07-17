16d6a5abe818bca2801b31b7c9e3452d544a0645
boooiil <boooiil@outlook.com>
2023-07-17 10:57:53 -0400

    remove unused contructor

--- src/main/java/dev/boooiil/historia/runnable/Expiry.java
+++ src/main/java/dev/boooiil/historia/runnable/Expiry.java
@@ -1,15 +1,8 @@
 package dev.boooiil.historia.runnable;
 
-import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class Expiry extends BukkitRunnable {
-
-    private final JavaPlugin plugin;
-
-    public Expiry(JavaPlugin plugin) {
-        this.plugin = plugin;
-    }
 
     @Override
     public void run() {
--- src/main/java/dev/boooiil/historia/runnable/PlayerIterator.java
+++ src/main/java/dev/boooiil/historia/runnable/PlayerIterator.java
@@ -1,6 +1,5 @@
 package dev.boooiil.historia.runnable;
 
-import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import dev.boooiil.historia.HistoriaPlugin;
@@ -8,12 +7,6 @@ import dev.boooiil.historia.classes.historia.user.HistoriaPlayer;
 import dev.boooiil.historia.database.internal.PlayerStorage;
 
 public class PlayerIterator extends BukkitRunnable {
-
-    private final JavaPlugin plugin;
-
-    public PlayerIterator(JavaPlugin plugin) {
-        this.plugin = plugin;
-    }
 
     @Override
     public void run() {
