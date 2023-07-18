baea750373424f2a8c06e9e21a04525cc0240811
boooiil <boooiil@outlook.com>
2023-07-18 04:55:24 -0400

    check ability to breed

---
+++ src/main/java/dev/boooiil/historia/events/mobs/EntityBreed.java
@@ -0,0 +1,32 @@
+package dev.boooiil.historia.events.mobs;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityBreedEvent;
+
+import dev.boooiil.historia.classes.enums.IncomeTypes.AnimalSources;
+import dev.boooiil.historia.classes.historia.user.HistoriaPlayer;
+import dev.boooiil.historia.database.internal.PlayerStorage;
+import dev.boooiil.historia.util.Logging;
+
+public class EntityBreed implements Listener {
+
+    @EventHandler
+    public void onBreedAnimal(EntityBreedEvent event) {
+    
+        // not sure what other entities can breed
+        // shouldn't (TM) be a problem
+        Player breeder = (Player) event.getBreeder();
+        HistoriaPlayer historiaPlayer = PlayerStorage.getPlayer(breeder.getUniqueId(), false);
+
+        if (historiaPlayer.getProficiency().getSkills().canTameAnimals()) {
+            historiaPlayer.increaseExperience(AnimalSources.BREED_ANIMAL.getKey());
+        }
+        else {
+            event.setCancelled(true);
+            Logging.infoToPlayer("Maybe there is someone else that can do this...", historiaPlayer.getUUID());
+        }
+    }
+
+}
