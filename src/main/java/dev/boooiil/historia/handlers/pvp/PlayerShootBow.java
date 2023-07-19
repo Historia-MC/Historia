13653c6b0f77573ba79e4e2f9319dc58c8393a7d
boooiil <boooiil@outlook.com>
2023-07-18 22:58:30 -0400

    validate if user can use bow

--- src/main/java/dev/boooiil/historia/handlers/pvp/PlayerShootBow.java
+++ src/main/java/dev/boooiil/historia/handlers/pvp/PlayerShootBow.java
@@ -4,6 +4,10 @@ import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityShootBowEvent;
 
+import dev.boooiil.historia.classes.historia.user.HistoriaPlayer;
+import dev.boooiil.historia.database.internal.PlayerStorage;
+import dev.boooiil.historia.util.Logging;
+
 public class PlayerShootBow {
     
     EntityShootBowEvent event;
@@ -17,8 +21,22 @@ public class PlayerShootBow {
         if (!(event.getEntity() instanceof Player)) return;
         if (!event.getConsumable().getItemMeta().hasEnchant(Enchantment.ARROW_FIRE)) return;
 
-        event.getProjectile().setFireTicks(1000);
+        HistoriaPlayer historiaPlayer = PlayerStorage.getPlayer(event.getEntity().getUniqueId(), false);
 
+        if (!historiaPlayer.getProficiency().canUseRanged()) {
+
+            event.setCancelled(true);
+            
+            // shouldn't need this, just a client bug if the items are missing
+            //((Player) event.getEntity()).getInventory().addItem(event.getConsumable());
+
+            Logging.infoToPlayer("You do not know how to use this weapon.", event.getEntity().getUniqueId());
+            return;
+
+        }
+
+        else {
+            event.getProjectile().setFireTicks(1000);
+        }
     }
-
 }
