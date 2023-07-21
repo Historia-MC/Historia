02031997a62d2422788f5701fab2c0f33c12a3f4
boooiil <boooiil@outlook.com>
2023-07-21 06:58:54 -0400

    reflect main class rename

--- src/main/java/dev/boooiil/historia/util/ConfigUtil.java
+++ src/main/java/dev/boooiil/historia/util/ConfigUtil.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 import org.bukkit.configuration.file.YamlConfiguration;
 
-import dev.boooiil.historia.HistoriaPlugin;
+import dev.boooiil.historia.Main;
 import dev.boooiil.historia.classes.enums.FileMap.ResourceKeys;
 
 public class ConfigUtil {
@@ -30,24 +30,24 @@ public class ConfigUtil {
         Logging.infoToConsole("Checking existance and version of config files.");
 
         for (String fileName : configFileNames) {
-            File diskFile = new File(HistoriaPlugin.plugin().getDataFolder(), fileName);
+            File diskFile = new File(Main.plugin().getDataFolder(), fileName);
 
             if (!diskFile.exists()) {
                 Logging.infoToConsole("Missing config file: " + fileName + " has been saved to disk from resources.");
                 Logging.infoToConsole("Location: " + diskFile.getAbsolutePath());
-                HistoriaPlugin.plugin().saveResource(fileName, false);
+                Main.plugin().saveResource(fileName, false);
                 continue;
             }
 
             YamlConfiguration diskConfig = yamlFromSource(diskFile);
-            YamlConfiguration jarConfig = yamlFromSource(HistoriaPlugin.plugin().getResource(fileName));
+            YamlConfiguration jarConfig = yamlFromSource(Main.plugin().getResource(fileName));
 
             int diskVersion = diskConfig.getInt("version");
             int jarVersion = jarConfig.getInt("version");
 
             if (diskVersion < jarVersion) { 
                 Logging.infoToConsole("Outdated config file (" + diskVersion + "): " + fileName + " has been replaced on disk by the newer version " + jarVersion + ".");
-                HistoriaPlugin.plugin().saveResource(fileName, true);
+                Main.plugin().saveResource(fileName, true);
                 continue;
             }
         }
