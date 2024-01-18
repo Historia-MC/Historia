75dbefa4df3f001c2d38fef765b5cec4af99c80b
boooiil <boooiil@outlook.com>
2024-01-17 19:35:31 -0500

    dont know why these were removed

---
+++ src/main/java/dev/boooiil/historia/core/handlers/blockBreakListener/PlayerPlaceBlock.java
@@ -0,0 +1,37 @@
+package dev.boooiil.historia.core.handlers.blockBreakListener;
+
+import dev.boooiil.historia.core.classes.enums.proficiency.SkillType;
+import dev.boooiil.historia.core.classes.user.HistoriaPlayer;
+import dev.boooiil.historia.core.database.internal.PlayerStorage;
+import dev.boooiil.historia.core.dependents.Permissions;
+import org.bukkit.Material;
+import org.bukkit.event.block.BlockPlaceEvent;
+
+public class PlayerPlaceBlock {
+
+    private final BlockPlaceEvent event;
+
+    public PlayerPlaceBlock(BlockPlaceEvent event) {
+
+        this.event = event;
+
+    }
+
+    public void doPlace() {
+
+        if (event.getBlock().getType() == Material.LADDER) {
+
+            HistoriaPlayer historiaPlayer = PlayerStorage.getPlayer(event.getPlayer().getUniqueId(), false);
+
+            if (!historiaPlayer.getProficiency().getSkills().hasSkill(SkillType.LADDER_BYPASS))
+                return;
+            if (Permissions.canPlaceBlock(event.getPlayer(), event.getBlock()))
+                return;
+
+            event.getPlayer().getWorld().setBlockData(event.getBlock().getLocation(),
+                    event.getBlock().getBlockData().clone());
+            event.getItemInHand().setAmount(event.getItemInHand().getAmount() - 1);
+
+        }
+    }
+}
