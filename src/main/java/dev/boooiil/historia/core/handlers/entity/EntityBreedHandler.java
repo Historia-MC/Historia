ee8a61f39d53ce4033b6add1dffe7f110ac1c63f
boooiil <boooiil@outlook.com>
2024-02-04 12:40:45 -0500

    handle animal breeding

---
+++ src/main/java/dev/boooiil/historia/core/handlers/entity/EntityBreedHandler.java
@@ -0,0 +1,62 @@
+package dev.boooiil.historia.core.handlers.entity;
+
+import org.bukkit.entity.Breedable;
+import org.bukkit.entity.Player;
+import org.bukkit.event.entity.EntityBreedEvent;
+
+import dev.boooiil.historia.core.database.internal.PlayerStorage;
+import dev.boooiil.historia.core.player.HistoriaPlayer;
+import dev.boooiil.historia.core.proficiency.experience.AnimalSources;
+import dev.boooiil.historia.core.proficiency.skills.SkillType;
+import dev.boooiil.historia.core.util.Logging;
+
+public class EntityBreedHandler {
+
+    EntityBreedEvent event;
+
+    public EntityBreedHandler(EntityBreedEvent event) {
+        this.event = event;
+    }
+
+    public void doDetermineBreedAnimalAndRunBreed() {
+
+        switch (event.getEntity().getType()) {
+            case PIG:
+            case COW:
+            case SHEEP:
+            case CHICKEN:
+            case RABBIT:
+            case TURTLE:
+            case FOX:
+            case WOLF:
+            case PARROT:
+            case OCELOT:
+                doBreedAnimal();
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    private void doBreedAnimal() {
+        // not sure what other entities can breed
+        // shouldn't (TM) be a problem
+        Player breeder = (Player) event.getBreeder();
+        HistoriaPlayer historiaPlayer = PlayerStorage.getPlayer(breeder.getUniqueId(), false);
+
+        if (!historiaPlayer.getProficiency().getSkills().hasSkill(SkillType.TAME_ANIMALS)) {
+            Logging.infoToPlayer("Unfortunately you were never told about the birds and bees.",
+                    historiaPlayer.getUUID());
+            event.setCancelled(true);
+
+            ((Breedable) event.getFather()).setBreed(false);
+            ((Breedable) event.getMother()).setBreed(false);
+
+            return;
+        }
+
+        historiaPlayer.increaseExperience(AnimalSources.BREED_ANIMAL.getKey());
+
+    }
+}
