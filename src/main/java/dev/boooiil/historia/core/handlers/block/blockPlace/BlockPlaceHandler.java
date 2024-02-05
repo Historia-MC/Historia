bdb6f0fca626741c56574b00bf2b43a6b7ded3fd
boooiil <boooiil@outlook.com>
2024-02-05 12:06:12 -0500

    implement chance to not consume block

--- src/main/java/dev/boooiil/historia/core/handlers/block/blockPlace/BlockPlaceHandler.java
+++ src/main/java/dev/boooiil/historia/core/handlers/block/blockPlace/BlockPlaceHandler.java
@@ -4,9 +4,12 @@ import dev.boooiil.historia.core.database.internal.PlayerStorage;
 import dev.boooiil.historia.core.dependents.Permissions;
 import dev.boooiil.historia.core.player.HistoriaPlayer;
 import dev.boooiil.historia.core.proficiency.skills.SkillType;
+import dev.boooiil.historia.core.util.Logging;
+import dev.boooiil.historia.core.util.NumberUtils;
 
 import org.bukkit.Material;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class BlockPlaceHandler {
 
@@ -27,6 +30,7 @@ public class BlockPlaceHandler {
                 break;
 
             default:
+                doConsumptionBypass();
                 break;
         }
     }
@@ -47,6 +51,43 @@ public class BlockPlaceHandler {
             event.getItemInHand().setAmount(event.getItemInHand().getAmount() - 1);
 
         }
+    }
+
+    private void doConsumptionBypass() {
+
+        // guard against players who cannot place the block
+        if (!Permissions.canPlaceBlock(event.getPlayer(), event.getBlock()))
+            return;
+
+        HistoriaPlayer historiaPlayer = PlayerStorage.getPlayer(event.getPlayer().getUniqueId(), false);
+
+        // guard against players that do not have this skill
+        if (!historiaPlayer.getProficiency().getSkills().hasSkill(SkillType.CHANCE_NO_CONSUME_BLOCK))
+            return;
+
+        switch (event.getBlock().getType()) {
+
+            case IRON_BLOCK:
+            case DIAMOND_BLOCK:
+            case EMERALD_BLOCK:
+            case GOLD_BLOCK:
+                return;
+
+            default:
+                break;
+        }
+
+        int chance = 1;
+        int random = NumberUtils.randomInt(1, 100);
+
+        if (random > chance)
+            return;
+
+        ItemStack item = new ItemStack(event.getBlock().getType(), 1);
+
+        event.getPlayer().getInventory().addItem(item);
+
+        Logging.infoToPlayer("Your skills allowed you to not consume a block!", historiaPlayer.getUUID());
     }
 
 }
