881adc4f92cef0f2f778bce2598178c50d60ffac
boooiil <boooiil@outlook.com>
2024-02-17 13:28:16 -0500

    explicit util imports

--- src/main/java/dev/boooiil/historia/core/database/mysql/MySQLHandler.java
+++ src/main/java/dev/boooiil/historia/core/database/mysql/MySQLHandler.java
@@ -8,7 +8,11 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 /**
  * It's a class that handles all the MySQL queries for the plugin.
