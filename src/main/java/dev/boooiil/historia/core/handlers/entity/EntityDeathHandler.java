9e02c9a5121028dfc37231013c2ad2858a982cd2
boooiil <boooiil@outlook.com>
2024-03-15 06:41:13 -0400

    increase xp for normal & range based kills

--- src/main/java/dev/boooiil/historia/core/handlers/entity/EntityDeathHandler.java
+++ src/main/java/dev/boooiil/historia/core/handlers/entity/EntityDeathHandler.java
@@ -6,25 +6,40 @@ import java.util.List;
 import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.inventory.ItemStack;
 
 import dev.boooiil.historia.core.database.internal.PlayerStorage;
 import dev.boooiil.historia.core.player.HistoriaPlayer;
 import dev.boooiil.historia.core.proficiency.experience.AnimalSources;
+import dev.boooiil.historia.core.proficiency.experience.CombatSources;
 import dev.boooiil.historia.core.proficiency.skills.Skills.SkillType;
 import dev.boooiil.historia.core.util.NumberUtils;
 
 public class EntityDeathHandler {
 
     EntityDeathEvent event;
+    DamageCause cause;
+    HistoriaPlayer historiaPlayerKiller;
+    HistoriaPlayer historiaPlayerVictim;
 
     public EntityDeathHandler(EntityDeathEvent event) {
         this.event = event;
+        this.cause = event.getEntity().getLastDamageCause().getCause();
+
+        if (event.getEntity().getType() == EntityType.PLAYER)
+            this.historiaPlayerVictim = PlayerStorage.getPlayer(event.getEntity().getUniqueId());
+
+        if (event.getEntity().getKiller().getType() == EntityType.PLAYER)
+            this.historiaPlayerKiller = PlayerStorage.getPlayer(event.getEntity().getKiller().getUniqueId());
     }
 
     public void doDetermineDeathCauseAndRunDeath() {
 
         switch (event.getEntity().getType()) {
+            case PLAYER:
+                doDetermineCombatExperience();
+                break;
             case COW:
             case SHEEP:
             case PIG:
@@ -52,6 +67,10 @@ public class EntityDeathHandler {
 
     private void doDetermineHarvestType() {
 
+        if (historiaPlayerKiller == null) {
+            return;
+        }
+
         switch (event.getEntity().getType()) {
             case COW:
             case SHEEP:
@@ -67,12 +86,34 @@ public class EntityDeathHandler {
 
     }
 
+    private void doDetermineCombatExperience() {
+
+        if (historiaPlayerKiller == null || historiaPlayerVictim == null) {
+            return;
+        }
+
+        // if the killer used a bow
+        if (cause == DamageCause.PROJECTILE) {
+            // if the player has the ranged kill income source
+            if (historiaPlayerKiller.getProficiency().getStats().hasIncomeSource(CombatSources.RANGED_KILL.getKey())) {
+                historiaPlayerKiller.increaseExperience(CombatSources.RANGED_KILL.getKey());
+            }
+        }
+        // if the player has the kill income source
+        else if (historiaPlayerKiller.getProficiency().getStats().hasIncomeSource(CombatSources.KILL.getKey())) {
+            historiaPlayerKiller.increaseExperience(CombatSources.KILL.getKey());
+        }
+
+        // death gives -2 experience at the moment
+        // that is why we are increasing
+        historiaPlayerVictim.increaseExperience(CombatSources.DEATH.getKey());
+    }
+
     private void doHarvestLeather() {
 
-        HistoriaPlayer historiaPlayer = PlayerStorage.getPlayer(event.getEntity().getKiller().getUniqueId());
         List<ItemStack> drops = event.getDrops();
 
-        if (!historiaPlayer.getProficiency().getSkills().hasSkill(SkillType.HARVEST_LEATHER)) {
+        if (!historiaPlayerKiller.getProficiency().getSkills().hasSkill(SkillType.HARVEST_LEATHER)) {
             List<ItemStack> newDrops = new ArrayList<>();
 
             for (ItemStack drop : drops) {
@@ -97,23 +138,19 @@ public class EntityDeathHandler {
 
     private void doHarvestBones() {
 
-        HistoriaPlayer historiaPlayer = PlayerStorage.getPlayer(event.getEntity().getKiller().getUniqueId());
-
-        if (!historiaPlayer.getProficiency().getSkills().hasSkill(SkillType.HARVEST_BONES)) {
+        if (!historiaPlayerKiller.getProficiency().getSkills().hasSkill(SkillType.HARVEST_BONES)) {
             return;
         }
 
         event.getDrops().add(new ItemStack(Material.BONE, NumberUtils.randomInt(1, 3)));
 
-        historiaPlayer.increaseExperience(AnimalSources.KILL_ANIMAL.getKey());
+        historiaPlayerKiller.increaseExperience(AnimalSources.KILL_ANIMAL.getKey());
 
     }
 
     private void doExtraFeathers() {
 
-        HistoriaPlayer historiaPlayer = PlayerStorage.getPlayer(event.getEntity().getKiller().getUniqueId());
-
-        if (!historiaPlayer.getProficiency().getSkills().hasSkill(SkillType.CHANCE_EXTRA_FEATHERS)) {
+        if (!historiaPlayerKiller.getProficiency().getSkills().hasSkill(SkillType.CHANCE_EXTRA_FEATHERS)) {
             return;
         }
 
