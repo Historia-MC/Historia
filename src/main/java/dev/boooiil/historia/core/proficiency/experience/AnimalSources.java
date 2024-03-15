3f9ba061170ef88351b3bf0bf88516a65eb8bf1c
boooiil <boooiil@outlook.com>
2024-03-15 05:23:20 -0400

    add kill animal to sources

--- src/main/java/dev/boooiil/historia/core/proficiency/experience/AnimalSources.java
+++ src/main/java/dev/boooiil/historia/core/proficiency/experience/AnimalSources.java
@@ -4,6 +4,7 @@ public enum AnimalSources {
 
     NONE(AllSources.NONE),
     BREED_ANIMAL(AllSources.BREED_ANIMAL),
+    KILL_ANIMAL(AllSources.KILL_ANIMAL),
     TAME_ANIMAL(AllSources.TAME_ANIMAL);
 
     private final AllSources key;
