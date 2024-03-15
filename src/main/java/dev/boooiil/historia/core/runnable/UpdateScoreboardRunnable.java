869ff7531323945ba5e254f061baf8f5c6c46cae
boooiil <boooiil@outlook.com>
2024-03-15 02:32:29 -0400

    format, use PlayerStorage#getPlayer(UUID)

--- src/main/java/dev/boooiil/historia/core/runnable/UpdateScoreboardRunnable.java
+++ src/main/java/dev/boooiil/historia/core/runnable/UpdateScoreboardRunnable.java
@@ -10,37 +10,42 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 public class UpdateScoreboardRunnable extends BukkitRunnable {
 
-    @Override
-    public void run() {
-        Bukkit.getOnlinePlayers().forEach(player -> {
+        @Override
+        public void run() {
+                Bukkit.getOnlinePlayers().forEach(player -> {
 
-            HistoriaPlayer historiaPlayer = PlayerStorage.getPlayer(player.getUniqueId(), false);
-            ScoreboardAdapter scoreboardAdapter = new ScoreboardAdapter();
+                        HistoriaPlayer historiaPlayer = PlayerStorage.getPlayer(player.getUniqueId());
+                        ScoreboardAdapter scoreboardAdapter = new ScoreboardAdapter();
 
-            scoreboardAdapter.createHeader(ChatColor.GOLD + "" + ChatColor.BOLD + "HISTORIA");
-            scoreboardAdapter.addLine(12, "");
-            scoreboardAdapter.addLine(11,
-                    ChatColor.AQUA + "Proficiency: " + ChatColor.GRAY + historiaPlayer.getProficiency().getName());
-            scoreboardAdapter.addLine(10, ChatColor.DARK_AQUA + "Level: " + ChatColor.GRAY + historiaPlayer.getLevel());
-            scoreboardAdapter.addLine(9, ChatColor.AQUA + "Health: " + ChatColor.GRAY
-                    + NumberUtils.roundDouble(player.getHealth(), 2) + "/" + historiaPlayer.getBaseHealth());
-            scoreboardAdapter.addLine(8,
-                    ChatColor.DARK_AQUA + "Hunger: " + ChatColor.GRAY
-                            + NumberUtils.roundDouble(player.getFoodLevel(), 2) + "/"
-                            + historiaPlayer.getProficiency().getStats().getBaseFood());
-            scoreboardAdapter.addLine(7, ChatColor.AQUA + "Experience: " + ChatColor.GRAY
-                    + historiaPlayer.getCurrentExperience() + "/" + historiaPlayer.getMaxExperience());
-            scoreboardAdapter.addLine(6,
-                    ChatColor.DARK_AQUA + "Temperature: " + ChatColor.GRAY + historiaPlayer.getCurrentTemperature());
-            scoreboardAdapter.addLine(5, ChatColor.AQUA + "Weight: " + ChatColor.GRAY + null);
-            scoreboardAdapter.addLine(4, ChatColor.DARK_AQUA + "Weapon Class: " + ChatColor.GRAY
-                    + historiaPlayer.getProficiency().getStats().getUsableWeaponTypes());
-            scoreboardAdapter.addLine(3, ChatColor.AQUA + "Armor Class: " + ChatColor.GRAY
-                    + historiaPlayer.getProficiency().getStats().getUsableArmorTypes());
+                        scoreboardAdapter.createHeader(ChatColor.GOLD + "" + ChatColor.BOLD + "HISTORIA");
+                        scoreboardAdapter.addLine(12, "");
+                        scoreboardAdapter.addLine(11,
+                                        ChatColor.AQUA + "Proficiency: " + ChatColor.GRAY
+                                                        + historiaPlayer.getProficiency().getName());
+                        scoreboardAdapter.addLine(10,
+                                        ChatColor.DARK_AQUA + "Level: " + ChatColor.GRAY + historiaPlayer.getLevel());
+                        scoreboardAdapter.addLine(9, ChatColor.AQUA + "Health: " + ChatColor.GRAY
+                                        + NumberUtils.roundDouble(player.getHealth(), 2) + "/"
+                                        + historiaPlayer.getBaseHealth());
+                        scoreboardAdapter.addLine(8,
+                                        ChatColor.DARK_AQUA + "Hunger: " + ChatColor.GRAY
+                                                        + NumberUtils.roundDouble(player.getFoodLevel(), 2) + "/"
+                                                        + historiaPlayer.getProficiency().getStats().getBaseFood());
+                        scoreboardAdapter.addLine(7, ChatColor.AQUA + "Experience: " + ChatColor.GRAY
+                                        + historiaPlayer.getCurrentExperience() + "/"
+                                        + historiaPlayer.getMaxExperience());
+                        scoreboardAdapter.addLine(6,
+                                        ChatColor.DARK_AQUA + "Temperature: " + ChatColor.GRAY
+                                                        + historiaPlayer.getCurrentTemperature());
+                        scoreboardAdapter.addLine(5, ChatColor.AQUA + "Weight: " + ChatColor.GRAY + null);
+                        scoreboardAdapter.addLine(4, ChatColor.DARK_AQUA + "Weapon Class: " + ChatColor.GRAY
+                                        + historiaPlayer.getProficiency().getStats().getUsableWeaponTypes());
+                        scoreboardAdapter.addLine(3, ChatColor.AQUA + "Armor Class: " + ChatColor.GRAY
+                                        + historiaPlayer.getProficiency().getStats().getUsableArmorTypes());
 
-            scoreboardAdapter.addToPlayer(player);
+                        scoreboardAdapter.addToPlayer(player);
 
-        });
-    }
+                });
+        }
 
 }
