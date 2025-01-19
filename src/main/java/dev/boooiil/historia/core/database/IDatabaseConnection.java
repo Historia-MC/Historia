6eb46e64dcf65c6c0caecc551c590a72b2eefc2e
boooiil <boooiil@outlook.com>
2025-01-19 02:17:23 -0500

    add fromString to database type enum

--- src/main/java/dev/boooiil/historia/core/database/IDatabaseConnection.java
+++ src/main/java/dev/boooiil/historia/core/database/IDatabaseConnection.java
@@ -7,8 +7,40 @@ public interface IDatabaseConnection {
     // TODO: voids become boolean
 
     public enum DatabaseType {
-        MYSQL,
-        SQLITE
+        MYSQL("mysql"),
+        SQLITE("sqlite"),
+        UNKNOWN("unknown");
+
+        private final String key;
+
+        DatabaseType(String key) {
+
+            this.key = key;
+
+        }
+
+        public String getKey() {
+
+            return this.key;
+
+        }
+
+        public static DatabaseType fromString(String key) {
+
+            for (DatabaseType type : DatabaseType.values()) {
+
+                if (type.getKey().equalsIgnoreCase(key)) {
+
+                    return type;
+
+                }
+
+            }
+
+            return UNKNOWN;
+
+        }
+
     }
 
     public Connection getConnection();
