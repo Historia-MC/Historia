564c12cd421205e66146fe628c9c9062b0b9833e
boooiil <boooiil@outlook.com>
2025-07-23 03:20:24 -0400

    add getter that accpepts RegistryHolder

--- src/main/java/dev/boooiil/historia/core/registry/RegistryHolder.java
+++ src/main/java/dev/boooiil/historia/core/registry/RegistryHolder.java
@@ -16,6 +16,38 @@ public class RegistryHolder {
     private final Registry<Registry<?>> holder = RegistryHolder.generateHolder();
 
     /**
+     * Register a new value to the registry holder.
+     * 
+     * @param key   The key to register the value under.
+     * @param value The value to register.
+     */
+    public RegistryHolder register(NamespacedKey key, Registry<?> value) {
+        holder.register(key, value);
+        return this;
+    }
+
+    /**
+     * Deregister an item configuration from the registry holder.
+     * 
+     * @param key The key to deregister.
+     */
+    public RegistryHolder deregister(NamespacedKey key) {
+        holder.deregister(key);
+        return this;
+    }
+
+    /**
+     * Update a value in the registry holder.
+     * 
+     * @param key   The key to update.
+     * @param value The new value.
+     */
+    public RegistryHolder update(NamespacedKey key, Registry<?> value) {
+        holder.update(key, value);
+        return this;
+    }
+
+    /**
      * Get a registry from the registry holder.
      * 
      * This value will be bound to the plugin through its namespace key.
@@ -37,6 +69,39 @@ public class RegistryHolder {
      * 
      * @param <T>      The type of the registry.
      * @param registry The registry holder.
+     * @param key      The namespaced key of the registry. ie:
+     *                 'plugin:crafting_recipes'
+     * @param type     The class of the registry value.
+     * @return Registry<T> | null if not found.
+     */
+    @Nullable
+    @SuppressWarnings("unchecked")
+    public static <T> Registry<T> get(RegistryHolder registryHolder, NamespacedKey key, Type type) {
+
+        Registry<?> raw = registryHolder.get(key, type);
+
+        if (raw == null) {
+            CoreLogger.errorToConsole("The registry with key " + key + " does not exist.");
+            return null;
+        }
+
+        if (!raw.getType().equals(type)) {
+            throw new IllegalArgumentException(
+                    "The registry with key " + key + " is not a Registry<" + type.getTypeName()
+                            + "> type but is a Registry<"
+                            + raw.getType().getTypeName() + ">.");
+        }
+
+        return (Registry<T>) raw;
+    }
+
+    /**
+     * Get a registry from a provided registry of registries.
+     * 
+     * This value will be bound to the plugin through its namespace key.
+     * 
+     * @param <T>      The type of the registry.
+     * @param registry The registry of registries.
      * @param key      The namespaced key of the registry. ie:
      *                 'plugin:crafting_recipes'
      * @param type     The class of the registry value.
