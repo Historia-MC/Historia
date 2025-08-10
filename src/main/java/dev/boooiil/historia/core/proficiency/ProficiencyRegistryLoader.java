e9bc98efc55295be776b6915330faa1b743809b4
boooiil <boooiil@outlook.com>
2025-08-10 10:31:32 -0400

    init registry loader

---
+++ src/main/java/dev/boooiil/historia/core/proficiency/ProficiencyRegistryLoader.java
@@ -0,0 +1,61 @@
+package dev.boooiil.historia.core.proficiency;
+
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+
+import dev.boooiil.historia.core.HistoriaCore;
+import dev.boooiil.historia.core.file.FileIO;
+import dev.boooiil.historia.core.file.FileKeys;
+import dev.boooiil.historia.core.proficiency.Proficiency.ProficiencyName;
+import dev.boooiil.historia.core.proficiency.stats.StatModifiers;
+
+public class ProficiencyRegistryLoader {
+
+    /**
+     * Load the proficiency registry.
+     */
+    public static void load() {
+
+        FileConfiguration config = FileIO.get(FileKeys.PROFICIENCY);
+
+        // Load all proficiencies from the configuration file
+        for (String key : config.getKeys(false)) {
+            if (key.equals("version"))
+                return;
+
+            ConfigurationSection section = config.getConfigurationSection(key);
+
+            ProficiencyName name = ProficiencyName.fromString(key);
+
+            if (name != ProficiencyName.NONE) {
+
+                Proficiency proficiency = new Proficiency(section);
+                HistoriaCore.proficiencyRegistry.register(HistoriaCore.getNamespacedKey(name.getKeyLowercase()),
+                        proficiency);
+
+                StatModifiers modifiers = new StatModifiers(section.getConfigurationSection("modifiers"));
+                HistoriaCore.statModifiersRegistry.register(HistoriaCore.getNamespacedKey(name.getKeyLowercase()),
+                        modifiers);
+
+            } else {
+                if (!HistoriaCore.proficiencyRegistry.contains(HistoriaCore.getNamespacedKey("none"))) {
+                    section = config.getConfigurationSection("none");
+                    HistoriaCore.proficiencyRegistry.register(HistoriaCore.getNamespacedKey("none"),
+                            new Proficiency(section));
+
+                    StatModifiers modifiers = new StatModifiers(section.getConfigurationSection("modifiers"));
+                    HistoriaCore.statModifiersRegistry.register(HistoriaCore.getNamespacedKey("none"), modifiers);
+                }
+            }
+        }
+    }
+
+    /**
+     * Reload the proficiency registry.
+     */
+    public static void reload() {
+        // This method is intentionally left empty as the registry is reloaded
+        // through the ProficiencyRegistry class.
+    }
+
+}
