2599ee25129d68b28fe4bec1895bfaf6ce4c7dd1
boooiil <boooiil@outlook.com>
2025-09-08 08:32:11 -0400

    add create method to satisfy interface

--- src/main/java/dev/boooiil/historia/core/proficiency/skills/ISkill.java
+++ src/main/java/dev/boooiil/historia/core/proficiency/skills/ISkill.java
@@ -1,25 +1,28 @@
 package dev.boooiil.historia.core.proficiency.skills;
 
-import dev.boooiil.historia.core.proficiency.skills.Skills.SkillName;
 import dev.boooiil.historia.core.proficiency.skills.Skills.SkillType;
 import dev.boooiil.historia.core.util.JSONSerializable;
 import org.bukkit.NamespacedKey;
+import org.bukkit.configuration.ConfigurationSection;
 
 public interface ISkill extends JSONSerializable {
     /**
      * Get the type of the skill.
+     * 
      * @return Type of the skill.
      */
     SkillType getType();
 
     /**
      * Get the name of the skill.
+     * 
      * @return Name of the skill.
      */
     NamespacedKey getName();
 
     /**
      * Execute the skill with a given set of supplied objects.
+     * 
      * @param skillSuppliers - Objects to be provided for this skill.
      */
     void execute(SkillSupplier<?>... skillSuppliers);
@@ -30,4 +33,6 @@ public interface ISkill extends JSONSerializable {
     void register();
 
     void deregister();
+
+    ISkill create(ConfigurationSection section);
 }
