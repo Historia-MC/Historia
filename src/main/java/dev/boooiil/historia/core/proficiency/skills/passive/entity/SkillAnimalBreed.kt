51dbcbd6f088039ad739d0b56e64c7cc31047c67
boooiil <boooiil@outlook.com>
2025-09-18 09:14:56 -0400

    init

---
+++ src/main/java/dev/boooiil/historia/core/proficiency/skills/passive/entity/SkillAnimalBreed.kt
@@ -0,0 +1,89 @@
+package dev.boooiil.historia.core.proficiency.skills.passive.entity
+
+import dev.boooiil.historia.core.HistoriaCore
+import dev.boooiil.historia.core.database.internal.PlayerStorage
+import dev.boooiil.historia.core.proficiency.skills.ISkill
+import dev.boooiil.historia.core.proficiency.skills.ISkillHandler
+import dev.boooiil.historia.core.proficiency.skills.SkillSupplier
+import dev.boooiil.historia.core.proficiency.skills.SkillType
+import org.bukkit.NamespacedKey
+import org.bukkit.configuration.ConfigurationSection
+import org.bukkit.entity.EntityType
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.entity.EntityBreedEvent
+
+class SkillAnimalBreed(section: ConfigurationSection) : ISkillHandler {
+    override val name: NamespacedKey
+    override val description: String = section.getString("description") ?: "No description provided."
+
+    /**
+     * Get the type of the skill.
+     *
+     * @return Type of the skill.
+     */
+    override val type: SkillType
+        get() = SkillType.PASSIVE
+
+    private val entities: Set<EntityType>
+
+    init {
+        require(section.contains("entity")) { "Key 'entity' must be specified." }
+
+        this.entities = section.getStringList("entity").map { entity ->
+            requireNotNull(EntityType.fromName(entity)) { "Invalid material specified $entity" }
+        }.toSet()
+
+        this.name = HistoriaCore.getNamespacedKey(section.name)
+
+    }
+
+    @EventHandler
+    fun handle(event: EntityBreedEvent) {
+        execute(SkillSupplier(event))
+    }
+
+    /**
+     * Execute the skill with a given set of supplied objects.
+     *
+     * @param skillSuppliers - Objects to be provided for this skill.
+     */
+    override fun execute(vararg skillSuppliers: SkillSupplier<*>) {
+
+        val event = skillSuppliers[0].get() as? EntityBreedEvent
+            ?: error("Expected PlayerItemHeldEvent, but got null or wrong type")
+
+        val entity = event.entity
+        val player = event.breeder as Player
+        val historiaPlayer = PlayerStorage.getPlayer(player.uniqueId)
+        val proficiency = HistoriaCore.PROFICIENCY_REGISTRY.get(historiaPlayer.proficiency.name)
+            ?: error("Tried to get proficiency for player ${historiaPlayer.username} but it did not exist.")
+        val hasLevel = historiaPlayer.level
+        val wantedLevel = proficiency.skills.get(this) ?: return
+
+        if (wantedLevel > hasLevel)
+            event.isCancelled = true
+
+        if (!entities.contains(entity.type))
+            event.isCancelled = true
+    }
+
+    /**
+     * Register to be used to handle when the skill executes.
+     */
+    override fun register() {
+        HistoriaCore.instance.registerEvent(this)
+    }
+
+    override fun deregister() {
+        TODO("Not yet implemented")
+    }
+
+    override fun create(section: ConfigurationSection): ISkill {
+        return SkillEntityDrop(section)
+    }
+
+    override fun toJSON(): String {
+        TODO("Not yet implemented")
+    }
+}
