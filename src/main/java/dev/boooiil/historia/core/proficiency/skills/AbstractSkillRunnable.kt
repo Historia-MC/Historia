6e3174843ac8de64c04d3411822819b96523122b
boooiil <boooiil@outlook.com>
2025-09-20 10:59:12 -0400

    implement helper methods

--- src/main/java/dev/boooiil/historia/core/proficiency/skills/AbstractSkillRunnable.kt
+++ src/main/java/dev/boooiil/historia/core/proficiency/skills/AbstractSkillRunnable.kt
@@ -1,6 +1,11 @@
 package dev.boooiil.historia.core.proficiency.skills
 
+import dev.boooiil.historia.core.HistoriaCore
+import dev.boooiil.historia.core.database.internal.PlayerStorage
+import dev.boooiil.historia.core.player.HistoriaPlayer
+import dev.boooiil.historia.core.proficiency.Proficiency
 import org.bukkit.NamespacedKey
+import org.bukkit.entity.Player
 import org.bukkit.scheduler.BukkitRunnable
 
 abstract class AbstractSkillRunnable : BukkitRunnable(), ISkillRunnable {
@@ -25,5 +30,22 @@ abstract class AbstractSkillRunnable : BukkitRunnable(), ISkillRunnable {
 
     override fun run() {
         throw UnsupportedOperationException()
+    }
+    
+    override fun hasSkill(historiaPlayer: HistoriaPlayer): Boolean {
+        return getProficiency(historiaPlayer)?.hasSkill(this) ?: false
+    }
+
+    override fun hasLevelRequirement(historiaPlayer: HistoriaPlayer): Boolean {
+        return (getProficiency(historiaPlayer)?.skills?.get(this) ?: 0) > historiaPlayer.level
+
+    }
+
+    override fun getHistoriaPlayer(player: Player): HistoriaPlayer {
+        return PlayerStorage.getPlayer(player.uniqueId)
+    }
+
+    override fun getProficiency(historiaPlayer: HistoriaPlayer): Proficiency? {
+        return HistoriaCore.PROFICIENCY_REGISTRY.get(historiaPlayer.proficiency.name)
     }
 }
