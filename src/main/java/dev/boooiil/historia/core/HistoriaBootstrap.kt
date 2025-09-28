4292698816aafa247fef550a022acc54b859b63b
Pieter <zotje3000@gmail.com>
2025-09-28 10:25:55 +0200

    add HistoriaBootstrap.kt and paper-plugin.yml

---
+++ src/main/java/dev/boooiil/historia/core/HistoriaBootstrap.kt
@@ -0,0 +1,15 @@
+package dev.boooiil.historia.core
+
+import dev.boooiil.historia.core.commands.commandProficiency
+import io.papermc.paper.plugin.bootstrap.BootstrapContext
+import io.papermc.paper.plugin.bootstrap.PluginBootstrap
+import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents
+
+class HistoriaBootstrap : PluginBootstrap {
+
+    override fun bootstrap(context: BootstrapContext) {
+        context.lifecycleManager.registerEventHandler(LifecycleEvents.COMMANDS) { commands ->
+            commands.registrar().register(commandProficiency)
+        }
+    }
+}
