5c851224038ec52c8ecd3095a6858f68f43d429e
Pieter <zotje3000@gmail.com>
2025-09-28 11:13:09 +0200

    add getArgumentOrDefault and getPlayersOrExecutor

--- src/main/java/dev/boooiil/historia/core/commands/CommandContextExtensions.kt
+++ src/main/java/dev/boooiil/historia/core/commands/CommandContextExtensions.kt
@@ -4,12 +4,30 @@ import com.mojang.brigadier.context.CommandContext
 import com.mojang.brigadier.exceptions.SimpleCommandExceptionType
 import io.papermc.paper.command.brigadier.CommandSourceStack
 import io.papermc.paper.command.brigadier.MessageComponentSerializer
+import io.papermc.paper.command.brigadier.argument.resolvers.selector.PlayerSelectorArgumentResolver
 import net.kyori.adventure.text.Component
 import org.bukkit.entity.Player
 
 inline fun <reified V> CommandContext<*>.getArgument(name: String): V =
     this.getArgument(name, V::class.java)
 
+inline fun <reified V> CommandContext<*>.getArgumentOrDefault(name: String, default: V): V {
+    return try {
+        this.getArgument<V>(name)
+    } catch (_: IllegalArgumentException) {
+        default
+    }
+}
+
 fun CommandContext<CommandSourceStack>.requirePlayerExecutor() = this.source.executor as? Player ?: throw SimpleCommandExceptionType(
     MessageComponentSerializer.message().serialize(Component.text("Command must be executed on or as a player!"))
 ).create()
+
+fun CommandContext<CommandSourceStack>.getPlayersOrExecutor(argumentName: String): List<Player> {
+    return try {
+        val playerSelector = this.getArgument<PlayerSelectorArgumentResolver>(argumentName)
+        playerSelector.resolve(this.source)
+    } catch (_: IllegalArgumentException) {
+        listOf(this.requirePlayerExecutor())
+    }
+}
