417ea188d63e382df9fb6d33d8d1a29507e2b9d5
Pieter <zotje3000@gmail.com>
2025-09-28 10:46:35 +0200

    add item HistoriaArgumentType

--- src/main/java/dev/boooiil/historia/core/commands/HistoriaArgumentTypes.kt
+++ src/main/java/dev/boooiil/historia/core/commands/HistoriaArgumentTypes.kt
@@ -1,5 +1,6 @@
 package dev.boooiil.historia.core.commands
 
+import dev.boooiil.historia.core.items.HistoriaItem
 import dev.boooiil.historia.core.proficiency.Proficiency
 import dev.boooiil.historia.core.proficiency.skills.ISkill
 import dev.boooiil.historia.core.registry.RegistryHolder
@@ -7,4 +8,5 @@ import dev.boooiil.historia.core.registry.RegistryHolder
 object HistoriaArgumentTypes {
     fun proficiency(): RegistryArgument<Proficiency> = RegistryArgument(RegistryHolder.PROFICIENCY_REGISTRY)
     fun skill(): RegistryArgument<ISkill> = RegistryArgument(RegistryHolder.SKILL_REGISTRY)
+    fun item(): RegistryArgument<HistoriaItem> = RegistryArgument(RegistryHolder.ITEM_REGISTRY)
 }
