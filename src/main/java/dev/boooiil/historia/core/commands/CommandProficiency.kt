178084acf71f411fee87077ed5d28233f264d488
Pieter <zotje3000@gmail.com>
2025-09-28 10:24:52 +0200

    update CommandProficiency.kt to use PlayerSelector

--- src/main/java/dev/boooiil/historia/core/commands/CommandProficiency.kt
+++ src/main/java/dev/boooiil/historia/core/commands/CommandProficiency.kt
@@ -3,9 +3,11 @@ package dev.boooiil.historia.core.commands
 import com.mojang.brigadier.tree.LiteralCommandNode
 import dev.boooiil.historia.core.player.HistoriaPlayer
 import dev.boooiil.historia.core.proficiency.Proficiency
+import dev.boooiil.historia.core.util.CoreLogger
 import io.papermc.paper.command.brigadier.CommandSourceStack
 import io.papermc.paper.command.brigadier.Commands
 import io.papermc.paper.command.brigadier.argument.ArgumentTypes
+import io.papermc.paper.command.brigadier.argument.resolvers.selector.PlayerSelectorArgumentResolver
 import org.bukkit.entity.Player
 
 val commandProficiency: LiteralCommandNode<CommandSourceStack> = Commands.literal("proficiency")
@@ -13,11 +15,14 @@ val commandProficiency: LiteralCommandNode<CommandSourceStack> = Commands.litera
         .then(Commands.argument("player", ArgumentTypes.player())
             .then(Commands.argument("proficiency", HistoriaArgumentTypes.proficiency())
                 .executes { ctx ->
-                    val player = ctx.getArgument<Player>("player")
+                    val playerSelector = ctx.getArgument<PlayerSelectorArgumentResolver>("player")
                     val proficiency = ctx.getArgument<Proficiency>("proficiency")
 
-                    val hPlayer = HistoriaPlayer(player.uniqueId)
-                    hPlayer.changeProficiency(proficiency.name)
+                    playerSelector.resolve(ctx.source).forEach { player ->
+                        val hPlayer = HistoriaPlayer(player.uniqueId)
+                        hPlayer.changeProficiency(proficiency.name)
+                        CoreLogger.infoToPlayer("Changed proficiency to ${proficiency.name}", player.uniqueId)
+                    }
                     return@executes 1
                 }
             )
