96d3ff243c9054389bff44eeb693cade3ee68a88
Pieter <zotje3000@gmail.com>
2025-09-28 11:13:50 +0200

    simplify give command and add optional amount

--- src/main/java/dev/boooiil/historia/core/commands/CommandGive.kt
+++ src/main/java/dev/boooiil/historia/core/commands/CommandGive.kt
@@ -1,5 +1,7 @@
 package dev.boooiil.historia.core.commands
 
+import com.mojang.brigadier.arguments.IntegerArgumentType
+import com.mojang.brigadier.context.CommandContext
 import com.mojang.brigadier.tree.LiteralCommandNode
 import dev.boooiil.historia.core.items.HistoriaItem
 import dev.boooiil.historia.core.player.HistoriaPlayer
@@ -9,29 +11,32 @@ import io.papermc.paper.command.brigadier.CommandSourceStack
 import io.papermc.paper.command.brigadier.Commands
 import io.papermc.paper.command.brigadier.argument.ArgumentTypes
 import io.papermc.paper.command.brigadier.argument.resolvers.selector.PlayerSelectorArgumentResolver
+import org.bukkit.entity.Player
 
 val commandGive: LiteralCommandNode<CommandSourceStack> = Commands.literal("give")
     .then(Commands.argument("player", ArgumentTypes.player())
         .then(Commands.argument("item", HistoriaArgumentTypes.item())
-            .executes { ctx ->
-                val playerSelector = ctx.getArgument<PlayerSelectorArgumentResolver>("player")
-                val item = ctx.getArgument<HistoriaItem>("item")
-
-                playerSelector.resolve(ctx.source).forEach { player ->
-                    val stack = item.createItemStack()
-                    player.inventory.addItem(stack)
-                }
-                return@executes 1
-            }
+            .executes(::executeGive)
+            .then(Commands.argument("amount", IntegerArgumentType.integer(1))
+                .executes(::executeGive)
+            )
         )
     )
     .then(Commands.argument("item", HistoriaArgumentTypes.item())
-        .executes { ctx ->
-            val player = ctx.requirePlayerExecutor()
-            val item = ctx.getArgument<HistoriaItem>("item")
-
-            val stack = item.createItemStack()
-            player.inventory.addItem(stack)
-            return@executes 1
-        }
+        .executes(::executeGive)
+        .then(Commands.argument("amount", IntegerArgumentType.integer(1))
+            .executes(::executeGive)
+        )
     ).build()
+
+private fun executeGive(ctx: CommandContext<CommandSourceStack>): Int {
+    val players = ctx.getPlayersOrExecutor("player")
+    val item = ctx.getArgument<HistoriaItem>("item")
+    val amount = ctx.getArgumentOrDefault("amount", 1)
+
+    players.forEach { player ->
+        val stack = item.createItemStack(amount)
+        player.inventory.addItem(stack)
+    }
+    return 1
+}
