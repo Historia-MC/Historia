e9555377c5d1ec227cb5c68829525ac388ed3235
Pieter <zotje3000@gmail.com>
2025-09-28 10:28:34 +0200

    change native command type to NamespacedKey

--- src/main/java/dev/boooiil/historia/core/commands/RegistryArgument.kt
+++ src/main/java/dev/boooiil/historia/core/commands/RegistryArgument.kt
@@ -2,13 +2,14 @@ package dev.boooiil.historia.core.commands
 
 import com.mojang.brigadier.StringReader
 import com.mojang.brigadier.arguments.ArgumentType
-import com.mojang.brigadier.arguments.StringArgumentType
 import com.mojang.brigadier.context.CommandContext
 import com.mojang.brigadier.exceptions.SimpleCommandExceptionType
 import com.mojang.brigadier.suggestion.Suggestions
 import com.mojang.brigadier.suggestion.SuggestionsBuilder
+import dev.boooiil.historia.core.HistoriaCore
 import dev.boooiil.historia.core.registry.Registry
 import io.papermc.paper.command.brigadier.MessageComponentSerializer
+import io.papermc.paper.command.brigadier.argument.ArgumentTypes
 import io.papermc.paper.command.brigadier.argument.CustomArgumentType
 import net.kyori.adventure.text.Component
 import org.bukkit.NamespacedKey
@@ -16,22 +17,18 @@ import java.util.concurrent.CompletableFuture
 
 class RegistryArgument<T : Any>(
     private val registry: Registry<T>
-) : CustomArgumentType<T, String> {
+) : CustomArgumentType<T, NamespacedKey> {
 
     override fun parse(reader: StringReader): T {
-        val input = reader.readUnquotedString()
-
-        val key = NamespacedKey.fromString(input) ?: throw SimpleCommandExceptionType(
-            MessageComponentSerializer.message().serialize(Component.text("$input is not a namespaced key!"))
-        ).create()
+        val key = ArgumentTypes.namespacedKey().parse(reader)
 
         return registry.get(key) ?: throw SimpleCommandExceptionType(
             MessageComponentSerializer.message().serialize(Component.text("Could not find key $key in registry!"))
         ).create()
     }
 
-    override fun getNativeType(): ArgumentType<String> {
-        return StringArgumentType.word()
+    override fun getNativeType(): ArgumentType<NamespacedKey> {
+        return ArgumentTypes.namespacedKey()
     }
 
     override fun <S : Any> listSuggestions(
