5f96f32a4bb40269fdfb399b8e2f83a30ad56be8
Pieter <zotje3000@gmail.com>
2025-09-28 15:04:53 +0200

    update proficiency scoreboard line

--- src/main/java/dev/boooiil/historia/core/util/ComponentExtensions.kt
+++ src/main/java/dev/boooiil/historia/core/util/ComponentExtensions.kt
@@ -1,6 +1,7 @@
 package dev.boooiil.historia.core.util
 
+import dev.boooiil.historia.core.scoreboard.ScoreboardBuilder
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.ComponentLike
 
-operator fun Component.plus(component: ComponentLike) = append(component)
+operator fun Component.plus(other: ComponentLike) = append(other)
