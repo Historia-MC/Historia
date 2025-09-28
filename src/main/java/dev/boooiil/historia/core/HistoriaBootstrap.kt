ca80cdd4f0f1d79f589b3c1c804047f21185e547
Pieter <zotje3000@gmail.com>
2025-09-28 15:36:11 +0200

    move command registration to Commands

--- src/main/java/dev/boooiil/historia/core/HistoriaBootstrap.kt
+++ src/main/java/dev/boooiil/historia/core/HistoriaBootstrap.kt
@@ -1,7 +1,6 @@
 package dev.boooiil.historia.core
 
-import dev.boooiil.historia.core.commands.commandGive
-import dev.boooiil.historia.core.commands.commandProficiency
+import dev.boooiil.historia.core.commands.Commands
 import io.papermc.paper.plugin.bootstrap.BootstrapContext
 import io.papermc.paper.plugin.bootstrap.PluginBootstrap
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents
@@ -9,9 +8,6 @@ import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents
 class HistoriaBootstrap : PluginBootstrap {
 
     override fun bootstrap(context: BootstrapContext) {
-        context.lifecycleManager.registerEventHandler(LifecycleEvents.COMMANDS) { commands ->
-            commands.registrar().register(commandProficiency)
-            commands.registrar().register(commandGive)
-        }
+        context.lifecycleManager.registerEventHandler(LifecycleEvents.COMMANDS) { commands -> Commands.register(commands) }
     }
 }
