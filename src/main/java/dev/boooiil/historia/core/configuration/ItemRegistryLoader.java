833e5c8005fef0c801613affff3315680a366e66
boooiil <boooiil@outlook.com>
2025-10-02 15:17:54 -0400

    make logging verbose

--- src/main/java/dev/boooiil/historia/core/configuration/ItemRegistryLoader.java
+++ src/main/java/dev/boooiil/historia/core/configuration/ItemRegistryLoader.java
@@ -40,7 +40,7 @@ public class ItemRegistryLoader {
      */
     @ApiStatus.Experimental
     public static void load(Supplier<List<YamlConfiguration>> configSupplier) {
-        CoreLogger.debugToConsole("Initializing ItemRegistryLoader...");
+        CoreLogger.verboseToConsole("Initializing ItemRegistryLoader...");
         configurations = configSupplier.get();
         populate(configurations);
     }
@@ -51,7 +51,7 @@ public class ItemRegistryLoader {
      * configurations with
      */
     public static void load() {
-        CoreLogger.debugToConsole("Initializing ItemRegistryLoader...");
+        CoreLogger.verboseToConsole("Initializing ItemRegistryLoader...");
         configurations = FileIO.loadYamlConfigurationsFromPlugins();
         populate(configurations);
     }
