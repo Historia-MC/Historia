b175f17583fabfa26c3eaa2c5b2ea4bbb0cb9457
boooiil <boooiil@outlook.com>
2025-10-02 17:57:06 -0400

    load filekeys as resource on plugin init

--- src/main/java/dev/boooiil/historia/core/file/FileKeys.java
+++ src/main/java/dev/boooiil/historia/core/file/FileKeys.java
@@ -18,27 +18,7 @@ public enum FileKeys {
      * skills.yml
      */
     SKILLS("skills.yml"),
-    /**
-     * armor.yml
-     */
-    ARMOR("armor.yml"),
-    /**
-     * weapons.yml
-     */
-    WEAPONS("weapons.yml"),
-    /**
-     * tools.yml
-     */
-    TOOLS("tools.yml"),
-    /**
-     * customitems.yml
-     */
-    CUSTOM_ITEMS("items.yml"),
-    /**
-     * ingots.yml
-     */
-    INGOTS("ingots.yml"),
-
+    
     TEMPERATURE("temperature.yml"),
 
     EXPIRY("expiry.yml"),
