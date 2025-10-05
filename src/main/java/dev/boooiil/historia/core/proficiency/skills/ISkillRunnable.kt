9492e0998704a7bb33a5ab2ee79c33ef1f13b1d9
boooiil <boooiil@outlook.com>
2025-10-05 16:11:12 -0400

    add BukkitRunnable fn

--- src/main/java/dev/boooiil/historia/core/proficiency/skills/ISkillRunnable.kt
+++ src/main/java/dev/boooiil/historia/core/proficiency/skills/ISkillRunnable.kt
@@ -1,5 +1,8 @@
 package dev.boooiil.historia.core.proficiency.skills
 
+import org.bukkit.scheduler.BukkitRunnable
+
 interface ISkillRunnable : ISkill, Runnable {
     override fun run()
+    fun runnable(): BukkitRunnable
 }
